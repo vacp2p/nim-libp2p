d685147c52b189d5da22e00c48b7fb4ea8a22ea8
cheatfate <eugene.kabanov@status.im>
2019-04-15 11:16:54 +0300

    Move ipnet/iface functionality to nim-chronos. Adjust tests.

--- tests/testiface.nim
+++
@@ -1,9 +0,0 @@
-import unittest
-import ../libp2p/ipnet/iface
-
-suite "OS interfaces list suite":
-  test "Get interfaces list test":
-    var ifaces = getInterfaces()
-    for item in ifaces:
-      echo item
-    check len(ifaces) > 0
--- tests/testnative.nim
+++ tests/testnative.nim
@@ -2,4 +2,3 @@ import unittest
 import testvarint, testbase32, testbase58, testbase64
 import testrsa, testecnist, tested25519, testcrypto
 import testmultibase, testmultihash, testmultiaddress, testcid, testpeer
-import testipnet, testiface
