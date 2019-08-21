685f7483f6b25aed73133f7f16e1bd98e943039d
Dmitriy Ryajov <dryajov@gmail.com>
2019-08-21 13:15:51 -0600

    feat: tcp transport listen

--- libp2p/tcptransport.nim
+++ libp2p/tcptransport.nim
@@ -11,21 +11,19 @@ import chronos
 import transport, wire, connection, multiaddress, connection, multicodec
 
 type TcpTransport* = ref object of Transport
-  fd*: AsyncFD
   server*: StreamServer
 
 proc connHandler(server: StreamServer,
                  client: StreamTransport): Future[Connection] {.gcsafe, async.} =
   let t: TcpTransport = cast[TcpTransport](server.udata)
-  let conn: Connection = newConnection(newAsyncStreamReader(client),
-                                       newAsyncStreamWriter(client))
+  let conn: Connection = newConnection(server, client)
   let connHolder: ConnHolder = ConnHolder(connection: conn,
                                           connFuture: t.handler(conn))
   t.connections.add(connHolder)
   result = conn
 
 proc connCb(server: StreamServer,
-                 client: StreamTransport) {.gcsafe, async.} =
+            client: StreamTransport) {.gcsafe, async.} =
   discard connHandler(server, client)
 
 method init*(t: TcpTransport) =
@@ -39,21 +37,9 @@ method listen*(t: TcpTransport): Future[void] {.async.} =
   let listenFuture: Future[void] = newFuture[void]()
   result = listenFuture
 
-  proc initTransport(server: StreamServer, fd: AsyncFD): StreamTransport {.gcsafe.} =
-    t.server = server
-    t.fd = fd
-    listenFuture.complete()
-
   ## listen on the transport
-  let server = createStreamServer(t.ma,
-                                  connCb,
-                                  {},
-                                  t,
-                                  asyncInvalidSocket,
-                                  100,
-                                  DefaultStreamBufferSize,
-                                  nil,
-                                  initTransport)
+  let server = createStreamServer(t.ma, connCb, {}, t)
+  t.server = server
   server.start()
 
 method dial*(t: TcpTransport,
