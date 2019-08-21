c15a9bdd670675cc319046ec575003484792daef
Dmitriy Ryajov <dryajov@gmail.com>
2019-08-21 16:53:16 -0600

    feat: adding dial tests

--- libp2p/connection.nim
+++ libp2p/connection.nim
@@ -14,19 +14,21 @@ const DefaultReadSize = 1024
 type
   Connection* = ref object of RootObj
     reader: AsyncStreamReader
-    writter: AsyncStreamWriter
+    writer: AsyncStreamWriter
     server: StreamServer
     client: StreamTransport
+    isOpen*: bool
 
 proc newConnection*(server: StreamServer,
                     client: StreamTransport): Connection = 
-  ## create a new Connection for the specified async stream reader/writter
+  ## create a new Connection for the specified async stream reader/writer
   new result
+  result.isOpen = false
   result.server = server
   result.client = client
 
   result.reader = newAsyncStreamReader(client)
-  result.writter = newAsyncStreamWriter(client)
+  result.writer = newAsyncStreamWriter(client)
 
 method read* (c: Connection, size: int = DefaultReadSize): Future[seq[byte]] {.base, async, gcsafe.} = 
   ## read DefaultReadSize (1024) bytes or `size` bytes if specified
@@ -34,12 +36,18 @@ method read* (c: Connection, size: int = DefaultReadSize): Future[seq[byte]] {.b
 
 method write* (c: Connection, data: pointer, size: int): Future[void] {.base, async.} = 
   ## write bytes pointed to by `data` up to `size` size
-  discard c.writter.write(data, size)
+  discard c.writer.write(data, size)
 
 method close* (c: Connection): Future[void] {.base, async.} = 
   ## close connection
-  ## TODO: figure out how to correctly close the streams and underlying resource
-  discard
+  await c.reader.closeWait()
+
+  await c.writer.finish()
+  await c.writer.closeWait()
+
+  await c.client.closeWait()
+  c.server.stop()
+  c.server.close()
 
 method getPeerInfo* (c: Connection): Future[PeerInfo] {.base, async.} = 
   ## get up to date peer info
