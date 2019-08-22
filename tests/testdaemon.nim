714010ba8563070a86410f0d3574b7ed609adb39
cheatfate <eugene.kabanov@status.im>
2019-08-22 09:53:05 +0300

    Enable simple test.

--- tests/testdaemon.nim
+++ tests/testdaemon.nim
@@ -129,13 +129,13 @@ proc pubsubTest(f: set[P2PDaemonFlags]): Future[bool] {.async.} =
     result = true
 
 when isMainModule:
-  # suite "libp2p-daemon test suite":
-  #   test "Simple spawn and get identity test":
-  #     check:
-  #       waitFor(identitySpawnTest()) == true
-  #   test "Connect/Accept peer/stream test":
-  #     check:
-  #       waitFor(connectStreamTest()) == true
+  suite "libp2p-daemon test suite":
+    test "Simple spawn and get identity test":
+      check:
+        waitFor(identitySpawnTest()) == true
+    # test "Connect/Accept peer/stream test":
+    #   check:
+    #     waitFor(connectStreamTest()) == true
   #   test "Provide CID test":
   #     check:
   #       waitFor(provideCidTest()) == true
@@ -145,4 +145,3 @@ when isMainModule:
   #   test "FloodSub test":
   #     check:
   #       waitFor(pubsubTest({PSFloodSub})) == true
-  echo "Exiting daemon test"
