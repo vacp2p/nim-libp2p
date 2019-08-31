ef7a0b41e48e5cc7ec5006006b3dff69b34bf4cb
Dmitriy Ryajov <dryajov@gmail.com>
2019-08-31 16:25:21 -0600

    add switch tests to native

--- tests/testnative.nim
+++ tests/testnative.nim
@@ -2,4 +2,4 @@ import unittest
 import testvarint, testbase32, testbase58, testbase64
 import testrsa, testecnist, tested25519, testsecp256k1, testcrypto
 import testmultibase, testmultihash, testmultiaddress, testcid, testpeer
-import testidentify, testtransport, testmultistream
+import testidentify, testtransport, testmultistream, testswitch
