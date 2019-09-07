f0cab6fe43f82ca46aa6d32473a7b8451674c5ae
Dmitriy Ryajov <dryajov@gmail.com>
2019-09-07 00:20:03 -0600

    reworked to make msg reading sequential

--- libp2p/muxers/mplex/coder.nim
+++ libp2p/muxers/mplex/coder.nim
@@ -18,33 +18,39 @@ import types,
 type
   Phase = enum Header, Size
 
-proc readHeader*(conn: Connection): Future[(uint, MessageType)] {.async, gcsafe.} = 
+proc readMplexVarint(conn: Connection): Future[uint] {.async, gcsafe.} =
   var
-    header: uint
+    varint: uint
     length: int
     res: VarintStatus
   var buffer = newSeq[byte](10)
   try:
     for i in 0..<len(buffer):
       await conn.readExactly(addr buffer[i], 1)
-      res = LP.getUVarint(buffer.toOpenArray(0, i), length, header)
+      res = LP.getUVarint(buffer.toOpenArray(0, i), length, varint)
       if res == VarintStatus.Success:
-        let (id, msg) = (header shr 3, MessageType(header and 0x7))
-        return (header shr 3, MessageType(header and 0x7))
+        return varint
     if res != VarintStatus.Success:
       buffer.setLen(0)
       return
-  except TransportIncompleteError:
+  except TransportIncompleteError, AsyncStreamIncompleteError:
     buffer.setLen(0)
-    raise newLPStreamIncompleteError()
 
-proc writeHeader*(conn: Connection,
-                  id: int,
+proc readMsg*(conn: Connection): Future[(uint, MessageType, seq[byte])] {.async, gcsafe.} = 
+  let header = await conn.readMplexVarint()
+  let dataLen = await conn.readMplexVarint()
+  var data: seq[byte]
+  if dataLen > 0.uint:
+    data = await conn.read(dataLen.int)
+  result = (header shr 3, MessageType(header and 0x7), data)
+
+proc writeMsg*(conn: Connection,
+                  id: uint,
                   msgType: MessageType, 
-                  size: int = 0) {.async, gcsafe.} =
+                  data: seq[byte] = @[]) {.async, gcsafe.} =
   ## write lenght prefixed
   var buf = initVBuffer()
-  buf.writeVarint((id.uint shl 3) or msgType.uint)
-  buf.writeVarint(size.uint) # size should be always sent
+  buf.writeVarint((id shl 3) or ord(msgType).uint)
+  buf.writeVarint(data.len().uint) # size should be always sent
   buf.finish()
-  await conn.write(buf.buffer)
+  await conn.write(buf.buffer & data)
