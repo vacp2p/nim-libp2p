41aad2e7e62750834a1cd10466dd994bb40b1fba
Dmitriy Ryajov <dryajov@gmail.com>
2019-09-11 10:14:20 -0600

    make peerId an Option[T]

--- libp2p/peerinfo.nim
+++ libp2p/peerinfo.nim
@@ -7,9 +7,10 @@
 ## This file may not be copied, modified, or distributed except according to
 ## those terms.
 
+import options
 import peer, multiaddress
 
 type PeerInfo* = object of RootObj
-  peerId*: PeerID
+  peerId*: Option[PeerID]
   addrs*: seq[MultiAddress]
   protocols*: seq[string]
