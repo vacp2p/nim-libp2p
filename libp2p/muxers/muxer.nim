5f2d9445459f49930ec6040a15516b02ca41560c
Dmitriy Ryajov <dryajov@gmail.com>
2019-09-11 15:06:42 -0600

    add muxer handler and properly set stream peerinfo

--- libp2p/muxers/muxer.nim
+++ libp2p/muxers/muxer.nim
@@ -7,12 +7,16 @@
 ## This file may not be copied, modified, or distributed except according to
 ## those terms.
 
-import chronos
+import chronos, chronicles
 import ../protocols/protocol, 
        ../connection
 
+logScope:
+  topic = "Muxer"
+
 type
   StreamHandler* = proc(conn: Connection): Future[void] {.gcsafe.}
+  MuxerHandler* = proc(muxer: Muxer): Future[void] {.gcsafe.}
 
   Muxer* = ref object of RootObj
     streamHandler*: StreamHandler
@@ -23,6 +27,7 @@ type
   MuxerProvider* = ref object of LPProtocol
     newMuxer*: MuxerCreator
     streamHandler*: StreamHandler
+    muxerHandler*: MuxerHandler
 
 method newStream*(m: Muxer, name: string = ""): Future[Connection] {.base, async, gcsafe.} = discard
 method close*(m: Muxer) {.base, async, gcsafe.} = discard
@@ -37,13 +42,32 @@ proc newMuxerProvider*(creator: MuxerCreator, codec: string): MuxerProvider {.gc
   result.init()
 
 method `=streamHandler`*(m: MuxerProvider, handler: StreamHandler) {.base, gcsafe.} = 
+  ## new stream (channels) handler
+  ##
+  ## triggered every time there is a new 
+  ## stream (channel) oppened over a muxed 
+  ## connection
+  ##
   m.streamHandler = handler
+
+method `=muxerHandler`*(m: MuxerProvider, handler: MuxerHandler) {.base, gcsafe.} = 
+  ## new muxer (muxed connections) handler
+  ##
+  ## triggered every time there is a new muxed 
+  ## connection created
+  ##
+  m.muxerHandler = handler
 
 method init(c: MuxerProvider) =
   proc handler(conn: Connection, proto: string) {.async, gcsafe, closure.} =
     let muxer = c.newMuxer(conn)
+    if not isNil(c.muxerHandler):
+      debug "CALLING MUXER HANDLER"
+      await c.muxerHandler(muxer)
+
     if not isNil(c.streamHandler):
       muxer.streamHandler = c.streamHandler
-      await muxer.handle()
+
+    await muxer.handle()
 
   c.handler = handler
