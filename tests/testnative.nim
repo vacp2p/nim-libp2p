fd1cec2a704fedf199800cfdcb766e56935fb51f
Dmitriy Ryajov <dryajov@gmail.com>
2019-09-23 17:02:13 -0600

    test: re-enable identify

--- tests/testnative.nim
+++ tests/testnative.nim
@@ -3,5 +3,4 @@ import testvarint, testbase32, testbase58, testbase64
 import testrsa, testecnist, tested25519, testsecp256k1, testcrypto
 import testmultibase, testmultihash, testmultiaddress, testcid, testpeer
 import testtransport, testmultistream, testbufferstream, 
-       testmplex, testswitch
-# import testidentify
+       testmplex, testidentify, testswitch
