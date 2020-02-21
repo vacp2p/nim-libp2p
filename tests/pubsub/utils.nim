0389fb4ca519b759aa6ce2bb56c1418f3d5d7586
Dmitriy Ryajov <dryajov@gmail.com>
2020-02-20 19:14:39 -0600

    implicitelly dial pubsub if enabled

--- tests/pubsub/utils.nim
+++ tests/pubsub/utils.nim
@@ -8,10 +8,10 @@ proc generateNodes*(num: Natural, gossip: bool = false): seq[Switch] =
     result.add(newStandardSwitch(gossip = gossip))
 
 proc subscribeNodes*(nodes: seq[Switch]) {.async.} =
-  var dials: seq[Future[void]]
+  var dials: seq[Future[Connection]]
   for dialer in nodes:
     for node in nodes:
       if dialer.peerInfo.peerId != node.peerInfo.peerId:
-        dials.add(dialer.subscribeToPeer(node.peerInfo))
+        dials.add(dialer.dial(node.peerInfo))
   await sleepAsync(100.millis)
   await allFutures(dials)
