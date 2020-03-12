93fcd718f67205e91b9b33e724f5697dddaeccda
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-03-13 01:18:36 +0900

    Add an optional global exception handler collector

--- libp2p/errors.nim
+++ libp2p/errors.nim
@@ -2,9 +2,20 @@ type
   LibP2PError* = object of CatchableError
 
 ## The following is just some diagnostic
-import chronicles
+when defined(collect_exceptions):
+  import json
 
-globalRaiseHook = proc(e: ref Exception): bool {.gcsafe, locks: 0.} =
-  debug "Exception raised!", typename = e.name, loc = e.getStackTraceEntries()[^1]
-  trace "Exception trace", stacktrace = e.getStackTrace()
-  return true
+  var
+      f: File
+  doAssert f.open("exceptions.json", fmAppend)
+
+  proc `%`(cstr: cstring): JsonNode = %($cstr)
+
+  globalRaiseHook = proc(e: ref Exception): bool {.gcsafe, locks: 0.} =
+    let
+      j = %*{
+        "name": $e.name,
+        "stacktrace": e.getStackTraceEntries()
+      }
+    f.writeLine(j)
+    return true
