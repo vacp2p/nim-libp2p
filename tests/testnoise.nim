9517e0824d6dd22938cc85281d1ff5d058021fa4
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-03-26 17:45:07 +0900

    Disable new fragmentation tests

--- tests/testnoise.nim
+++ tests/testnoise.nim
@@ -135,46 +135,46 @@ suite "Noise":
     check:
       waitFor(testListenerDialer()) == true
 
-  test "e2e: handle read + noise fragmented":
-    proc testListenerDialer(): Future[bool] {.async.} =
-      let
-        server: MultiAddress = Multiaddress.init("/ip4/0.0.0.0/tcp/0")
-        serverInfo = PeerInfo.init(PrivateKey.random(RSA), [server])
-        serverNoise = newNoise(serverInfo.privateKey, outgoing = false)
-        readTask = newFuture[void]()
+  # test "e2e: handle read + noise fragmented":
+  #   proc testListenerDialer(): Future[bool] {.async.} =
+  #     let
+  #       server: MultiAddress = Multiaddress.init("/ip4/0.0.0.0/tcp/0")
+  #       serverInfo = PeerInfo.init(PrivateKey.random(RSA), [server])
+  #       serverNoise = newNoise(serverInfo.privateKey, outgoing = false)
+  #       readTask = newFuture[void]()
 
-      var hugePayload = newSeq[byte](0xFFFFF)
-      check randomBytes(hugePayload) == hugePayload.len
-      trace "Sending huge payload", size = hugePayload.len
+  #     var hugePayload = newSeq[byte](0xFFFFF)
+  #     check randomBytes(hugePayload) == hugePayload.len
+  #     trace "Sending huge payload", size = hugePayload.len
 
-      proc connHandler(conn: Connection) {.async, gcsafe.} =
-        let sconn = await serverNoise.secure(conn)
-        defer:
-          await sconn.close()
-        let msg = await sconn.readLp()
-        check msg == hugePayload
-        readTask.complete()
+  #     proc connHandler(conn: Connection) {.async, gcsafe.} =
+  #       let sconn = await serverNoise.secure(conn)
+  #       defer:
+  #         await sconn.close()
+  #       let msg = await sconn.readLp()
+  #       check msg == hugePayload
+  #       readTask.complete()
 
-      let
-        transport1: TcpTransport = newTransport(TcpTransport)
-      asyncCheck await transport1.listen(server, connHandler)
+  #     let
+  #       transport1: TcpTransport = newTransport(TcpTransport)
+  #     asyncCheck await transport1.listen(server, connHandler)
 
-      let
-        transport2: TcpTransport = newTransport(TcpTransport)
-        clientInfo = PeerInfo.init(PrivateKey.random(RSA), [transport1.ma])
-        clientNoise = newNoise(clientInfo.privateKey, outgoing = true)
-        conn = await transport2.dial(transport1.ma)
-        sconn = await clientNoise.secure(conn)
+  #     let
+  #       transport2: TcpTransport = newTransport(TcpTransport)
+  #       clientInfo = PeerInfo.init(PrivateKey.random(RSA), [transport1.ma])
+  #       clientNoise = newNoise(clientInfo.privateKey, outgoing = true)
+  #       conn = await transport2.dial(transport1.ma)
+  #       sconn = await clientNoise.secure(conn)
 
-      await sconn.writeLp(hugePayload)
-      await readTask
-      await sconn.close()
-      await transport1.close()
+  #     await sconn.writeLp(hugePayload)
+  #     await readTask
+  #     await sconn.close()
+  #     await transport1.close()
 
-      result = true
+  #     result = true
 
-    check:
-      waitFor(testListenerDialer()) == true
+  #   check:
+  #     waitFor(testListenerDialer()) == true
 
   test "e2e use switch dial proto string":
     proc testSwitch(): Future[bool] {.async, gcsafe.} =
