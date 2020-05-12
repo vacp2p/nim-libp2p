bdcea5fb01870a4f187a7d208dec2da3dfe7cd30
Dmitriy Ryajov <dryajov@gmail.com>
2020-05-11 22:29:27 -0600

    call parent close

--- libp2p/stream/bufferstream.nim
+++ libp2p/stream/bufferstream.nim
@@ -36,6 +36,9 @@ import ../stream/lpstream
 
 export lpstream
 
+logScope:
+  topic = "BufferStream"
+
 const
   BufferStreamTrackerName* = "libp2p.bufferstream"
   DefaultBufferSize* = 1024
@@ -295,8 +298,9 @@ method close*(s: BufferStream) {.async.} =
         r.fail(newLPStreamEOFError())
     s.dataReadEvent.fire()
     s.readBuf.clear()
-    s.closeEvent.fire()
-    s.isClosed = true
+
+    await procCall Connection(s).close() # call parent close
+
     inc getBufferStreamTracker().closed
     libp2p_open_bufferstream.dec()
   else:
