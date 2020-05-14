7cc12a790d92c22edbb0947c21627bcc252865d2
Dmitriy Ryajov <dryajov@gmail.com>
2020-05-14 16:26:08 -0600

    misc: comment out tracker dumps

--- tests/testmplex.nim
+++ tests/testmplex.nim
@@ -21,6 +21,7 @@ when defined(nimHasUsed): {.used.}
 suite "Mplex":
   teardown:
     for tracker in testTrackers():
+      # echo tracker.dump()
       check tracker.isLeaked() == false
 
   test "encode header with channel id 0":
