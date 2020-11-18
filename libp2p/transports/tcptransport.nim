46da7a98cb0442658060551a1713ea17316d4b52
Dmitriy Ryajov <dryajov@gmail.com>
2020-11-18 07:56:22 -0600

    Update libp2p/transports/tcptransport.nim
    Co-authored-by: Jacek Sieka <jacek@status.im>

--- libp2p/transports/tcptransport.nim
+++ libp2p/transports/tcptransport.nim
@@ -7,7 +7,7 @@
 ## This file may not be copied, modified, or distributed except according to
 ## those terms.
 
-import oids, sequtils
+import std/[oids, sequtils]
 import chronos, chronicles
 import transport,
        ../errors,
