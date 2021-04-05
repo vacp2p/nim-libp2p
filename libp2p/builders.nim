3b94b3a0a1c36ca905ce17768f84c72f64603fe8
Dmitriy Ryajov <dryajov@gmail.com>
2021-04-05 16:06:45 -0600

    builders (#559)

--- libp2p/builders.nim
+++ libp2p/builders.nim
@@ -29,8 +29,6 @@ type
     mplexOpts: MplexOpts
     tcpTransportOpts: TcpTransportOpts
     rng: ref BrHmacDrbgContext
-    inTimeout: Duration
-    outTimeout: Duration
     maxConnections: int
     maxIn: int
     maxOut: int
