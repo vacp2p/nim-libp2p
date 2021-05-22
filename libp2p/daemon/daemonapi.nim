24132d71294afe3b22b2792601be6dc1ff3441db
Dmitriy Ryajov <dryajov@gmail.com>
2021-05-22 12:27:30 -0600

    More raises cleanup (#575)
    * use toException to map errors
    
    * don't initialize address twice
    
    * better error messages
    
    * allow LPError to escape
    
    * allow LPError to escape

--- libp2p/daemon/daemonapi.nim
+++ libp2p/daemon/daemonapi.nim
@@ -11,12 +11,13 @@
 
 ## This module implementes API for `go-libp2p-daemon`.
 import std/[os, osproc, strutils, tables, strtabs]
-import chronos, chronicles
+import pkg/[chronos, chronicles]
 import ../varint, ../multiaddress, ../multicodec, ../cid, ../peerid
 import ../wire, ../multihash, ../protobuf/minprotobuf
 import ../crypto/crypto
 
-export peerid, multiaddress, multicodec, multihash, cid, crypto, wire
+export
+  peerid, multiaddress, multicodec, multihash, cid, crypto, wire
 
 when not defined(windows):
   import posix
@@ -154,6 +155,9 @@ type
                             ticket: PubsubTicket,
                             message: PubSubMessage): Future[bool] {.gcsafe.}
 
+  # TODO: would be nice to be able to map other errors to
+  # this types with `Result.toException`, but it doesn't work
+  # in this module
   DaemonRemoteError* = object of CatchableError
   DaemonLocalError* = object of CatchableError
 
@@ -833,15 +837,19 @@ proc getPeerInfo(pb: var ProtoBuffer): PeerInfo
   result.addresses = newSeq[MultiAddress]()
   if pb.getValue(1, result.peer) == -1:
     raise newException(DaemonLocalError, "Missing required field `peer`!")
+
   var address = newSeq[byte]()
   while pb.getBytes(2, address) != -1:
     if len(address) != 0:
       var copyaddr = address
       let addrRes = MultiAddress.init(copyaddr)
+
+      # TODO: for some reason `toException` doesn't
+      # work for this module
       if addrRes.isErr:
         raise newException(DaemonLocalError, addrRes.error)
 
-      result.addresses.add(MultiAddress.init(copyaddr).get())
+      result.addresses.add(addrRes.get())
       address.setLen(0)
 
 proc identity*(api: DaemonAPI): Future[PeerInfo] {.async.} =
