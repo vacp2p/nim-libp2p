5f17245ba9da02b2aca061d821158c045f0fe33c
Mark Spanbroek <mark@spanbroek.net>
2021-05-31 10:39:18 +0200

    Simplify test

--- tests/testquic.nim
+++ tests/testquic.nim
@@ -13,36 +13,43 @@ suite "QUIC transport":
 
   setup:
     transport = QuicTransport.new()
+    await transport.start(address)
 
   teardown:
     await transport.stop()
+
+  template incoming(stream, body) =
+    let session = await transport.accept()
+    let stream = await session.getStream()
+    body
+    await stream.close()
+    await session.close()
+
+  template outgoing(stream, body) =
+    let connection = await dial(initTAddress(address).tryGet)
+    let stream = await connection.openStream()
+    body
+    await stream.close()
+    await connection.waitClosed()
 
   test "handles QUIC addresses":
     let tcpAddress = MultiAddress.init("/ip4/127.0.0.1/tcp/45894").get()
     check transport.handles(address) == true
     check transport.handles(tcpAddress) == false
 
-
   test "handles incoming stream":
-
     const message = "test".toBytes()
 
     proc read {.async.} =
-      await transport.start(address)
-      let session = await transport.accept()
-      let stream = await session.getStream()
-      var buffer: array[message.len, byte]
-      await stream.readExactly(addr buffer[0], buffer.len)
-      check @buffer == message
-      await stream.close()
-      await session.close()
+      incoming stream:
+        var buffer: array[message.len, byte]
+        await stream.readExactly(addr buffer[0], buffer.len)
+        check @buffer == message
 
     let reading = read()
 
-    let connection = await dial(initTAddress(address).tryGet)
-    let stream = await connection.openStream()
-    await stream.write(message)
-    await stream.close()
-    await connection.waitClosed()
+    outgoing stream:
+      await stream.write(message)
 
     await reading
+
