58f9d12d5358e6634e7c6fddc8e304282ea87532
Tanguy Cizain <tanguycizain@gmail.com>
2021-06-29 18:13:02 +0200

    renamed ws to websock

--- libp2p/transports/wstransport.nim
+++ libp2p/transports/wstransport.nim
@@ -20,7 +20,7 @@ import transport,
        ../multiaddress,
        ../stream/connection,
        ../upgrademngrs/upgrade,
-       ws/ws
+       websock/websock
 
 logScope:
   topics = "libp2p wstransport"
