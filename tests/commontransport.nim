d8fe5701ae2e95c2ad21bafa34b5a8244207ad9d
Tanguy Cizain <tanguycizain@gmail.com>
2021-06-30 10:13:54 +0200

    add common transport test

--- tests/commontransport.nim
+++ tests/commontransport.nim
@@ -91,3 +91,50 @@ proc commonTransportTest*(transportType: typedesc[Transport], ma: string) =
       check acceptHandler.cancelled
 
       await transport1.stop()
+
+    asyncTest "e2e should allow multiple local addresses":
+      let addrs = @[MultiAddress.init(ma).tryGet(),
+                    MultiAddress.init(ma).tryGet()]
+
+
+      let transport1 = transportType.new(upgrade = Upgrade())
+      await transport1.start(addrs)
+
+      proc acceptHandler() {.async, gcsafe.} =
+        while true:
+          let conn = await transport1.accept()
+          await conn.write("Hello!")
+          await conn.close()
+
+      let handlerWait = acceptHandler()
+
+      check transport1.addrs.len == 2
+      check transport1.addrs[0] != transport1.addrs[1]
+
+      var msg = newSeq[byte](6)
+
+      proc client(ma: MultiAddress) {.async.} =
+        let conn1 = await transport1.dial(ma)
+        await conn1.readExactly(addr msg[0], 6)
+        check string.fromBytes(msg) == "Hello!"
+        await conn1.close()
+
+      #Dial the same server multiple time in a row
+      await client(transport1.addrs[0])
+      await client(transport1.addrs[0])
+      await client(transport1.addrs[0])
+
+      #Dial the same server on different addresses
+      await client(transport1.addrs[1])
+      await client(transport1.addrs[0])
+      await client(transport1.addrs[1])
+
+      #Cancel a dial
+      let
+        dial1 = transport1.dial(transport1.addrs[1])
+        dial2 = transport1.dial(transport1.addrs[0])
+      await dial1.cancelAndWait()
+      await dial2.cancelAndWait()
+
+      await handlerWait.cancelAndWait()
+      await transport1.stop()
