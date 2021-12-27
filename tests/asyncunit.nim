fb0d10b6fdd93515508de095c7f63f2547a5116b
Tanguy <tanguy@status.im>
2021-12-27 11:17:00 +0100

    Gossipsub: process messages concurrently (#680)
    * Gossip sub: process messages concurrently
    
    * Retries for flaky test

--- tests/asyncunit.nim
+++ tests/asyncunit.nim
@@ -22,3 +22,25 @@ template asyncTest*(name: string, body: untyped): untyped =
       proc() {.async, gcsafe.} =
         body
     )())
+
+template flakyAsyncTest*(name: string, attempts: int, body: untyped): untyped =
+  test name:
+    var attemptNumber = 0
+    while attemptNumber < attempts:
+      let isLastAttempt = attemptNumber == attempts - 1
+      inc attemptNumber
+      try:
+        waitFor((
+          proc() {.async, gcsafe.} =
+            body
+        )())
+      except Exception as e:
+        if isLastAttempt: raise e
+        else: testStatusIMPL = TestStatus.FAILED
+      finally:
+        if not isLastAttempt:
+          if testStatusIMPL == TestStatus.FAILED:
+            # Retry
+            testStatusIMPL = TestStatus.OK
+          else:
+            break
