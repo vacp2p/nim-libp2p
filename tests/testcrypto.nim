db08cc97e80b6595302d158c236c96ba38bb54d5
Dustin Brody <tersec@users.noreply.github.com>
2022-04-11 11:26:24 +0000

    encrypt and decrypt empty sequences

--- tests/testcrypto.nim
+++ tests/testcrypto.nim
@@ -483,6 +483,17 @@ suite "Key interface test suite":
     ChaChaPoly.decrypt(key, nonce, btag, smallPlain, noaed)
     check ntag.toHex == btag.toHex
 
+    # ensure even a 0 byte array works
+    block:
+      var
+        emptyPlain: array[0, byte]
+        btag: ChaChaPolyTag
+        noaed: array[0, byte]
+      ChaChaPoly.encrypt(key, nonce, btag, emptyPlain, noaed)
+      ntag = btag
+      ChaChaPoly.decrypt(key, nonce, btag, emptyPlain, noaed)
+      check ntag.toHex == btag.toHex
+
   test "Curve25519":
     # from bearssl test_crypto.c
     var
