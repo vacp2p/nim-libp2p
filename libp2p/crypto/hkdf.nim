bcab332269a550aa05d6ce810c14dab9a7d0f902
Tanguy <tanguy@status.im>
2022-06-09 15:45:17 +0200

    fix every copyright header

--- libp2p/crypto/hkdf.nim
+++ libp2p/crypto/hkdf.nim
@@ -1,11 +1,11 @@
-## Nim-LibP2P
-## Copyright (c) 2020 Status Research & Development GmbH
-## Licensed under either of
-##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
-##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
-## at your option.
-## This file may not be copied, modified, or distributed except according to
-## those terms.
+# Nim-LibP2P
+# Copyright (c) 2022 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
 
 # https://tools.ietf.org/html/rfc5869
 
