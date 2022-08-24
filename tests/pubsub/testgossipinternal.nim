c594eadfca5160fb2484a8e62d19f8d002e6f6d2
Tanguy <tanguy@status.im>
2022-08-24 10:33:41 +0200

    fix test

--- tests/pubsub/testgossipinternal.nim
+++ tests/pubsub/testgossipinternal.nim
@@ -22,10 +22,7 @@ proc getPubSubPeer(p: TestGossipSub, peerId: PeerId): PubSubPeer =
   proc getConn(): Future[Connection] =
     p.switch.dial(peerId, GossipSubCodec)
 
-  proc dropConn(peer: PubSubPeer) =
-    discard # we don't care about it here yet
-
-  let pubSubPeer = PubSubPeer.new(peerId, getConn, dropConn, nil, GossipSubCodec, 1024 * 1024)
+  let pubSubPeer = PubSubPeer.new(peerId, getConn, nil, GossipSubCodec, 1024 * 1024)
   debug "created new pubsub peer", peerId
 
   p.peers[peerId] = pubSubPeer
