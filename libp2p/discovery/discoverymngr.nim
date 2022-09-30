8a28ad0648d158b342ef8d4aad8196bada22daff
Ludovic Chenut <ludovic@status.im>
2022-09-30 17:28:58 +0200

    [skip ci] merge interface in mngr & renaming

--- libp2p/discovery/discoverymngr.nim
+++ libp2p/discovery/discoverymngr.nim
@@ -19,36 +19,99 @@ import ./discoveryinterface,
 export discoveryinterface
 
 type
+  BaseAttr = ref object of RootObj
+    comparator*: proc(f: BaseAttr, c: BaseAttr): bool {.gcsafe, raises: [Defect].}
+
+  Attribute[T] = ref object of BaseAttr
+    value*: T
+
+  PeerAttributes* = object
+    attributes: seq[BaseAttr]
+
+  DiscoveryService* = distinct string
+
+proc `==`*(a, b: DiscoveryService): bool {.borrow.}
+
+proc ofType*[T](f: BaseAttr, _: type[T]): bool =
+  return f is Attribute[T]
+
+proc to*[T](f: BaseAttr, _: type[T]): T =
+  Attribute[T](f).value
+
+proc add*[T](pa: var PeerAttributes,
+             value: T) =
+  pa.attributes.add(Attribute[T](
+      value: value,
+      comparator: proc(f: BaseAttr, c: BaseAttr): bool =
+        f.ofType(T) and c.ofType(T) and f.to(T).value == c.to(T).value
+    )
+  )
+
+iterator items*(pa: PeerAttributes): BaseAttr =
+  for f in pa.attributes:
+    yield f
+
+proc `[]`*[T](pa: PeerAttributes, t: typedesc[T]): seq[T] =
+  for f in pa.attributes:
+    if f.ofType(T)
+      result.add(f.to(T).value)
+
+proc match*(pa, candidate: PeerAttributes): bool =
+  for f in pa.attributes:
+    block oneAttribute:
+      for field in candidate.attributes:
+        if field.comparator(field, f):
+          break oneAttribute
+      return false
+  return true
+
+type
+  PeerFoundCallback* = proc(pa: PeerAttributes) {.raises: [Defect], gcsafe.}
+
+  DiscoveryInterface* = ref object of RootObj
+    onPeerFound*: PeerFoundCallback
+    toAdvertise*: PeerAttributes
+    advertisementUpdated*: AsyncEvent
+    advertiseLoop*: Future[void]
+
+method request*(self: DiscoveryInterface, pa: PeerAttributes) {.async, base.} =
+  doAssert(false, "Not implemented!")
+
+method advertise*(self: DiscoveryInterface) {.async, base.} =
+  doAssert(false, "Not implemented!")
+
+type
   DiscoveryError* = object of LPError
+
+  DiscoveryQuery* = ref object
+    attr: PeerAttributes
+    peers: AsyncQueue[PeerAttributes]
+    futs: seq[Future[void]]
+
   DiscoveryManager* = ref object
     interfaces: seq[DiscoveryInterface]
     queries: seq[DiscoveryQuery]
 
-  DiscoveryQuery* = ref object
-    filter: DiscoveryFilters
-    peers: AsyncQueue[DiscoveryFilters]
-    futs: seq[Future[void]]
-
 proc add*(dm: DiscoveryManager, di: DiscoveryInterface) =
   dm.interfaces &= di
 
-  di.onPeerFound = proc (res: DiscoveryFilters) =
+  di.onPeerFound = proc (pa: PeerAttributes) =
     for query in dm.queries:
-      if query.filter.match(res):
+      if query.attr.match(pa):
         try:
-          query.peers.putNoWait(res)
+          query.peers.putNoWait(pa)
         except AsyncQueueFullError as exc:
           debug "Cannot push discovered peer to queue"
 
-proc request*(dm: DiscoveryManager, filter: DiscoveryFilters): DiscoveryQuery =
-  var query = DiscoveryQuery(filter: filter, peers: newAsyncQueue[DiscoveryFilters]())
+proc request*(dm: DiscoveryManager, pa: PeerAttributes): DiscoveryQuery =
+  var query = DiscoveryQuery(attr: pa, peers: newAsyncQueue[PeerAttributes]())
   for i in dm.interfaces:
-    query.futs.add(i.request(filter))
+    query.futs.add(i.request(pa))
   return query
 
-proc advertise*(dm: DiscoveryManager, filter: DiscoveryFilters) {.async.} =
+proc advertise*(dm: DiscoveryManager, pa: PeerAttributes) {.async.} =
   for i in dm.interfaces:
-    i.toAdvertise = filter
+    i.toAdvertise = pa
     if i.advertiseLoop.isNil:
       i.advertisementUpdated = newAsyncEvent()
       i.advertiseLoop = i.advertise()
@@ -60,7 +123,7 @@ proc stop*(dm: DiscoveryManager) =
     if isNil(i.advertiseLoop): continue
     i.advertiseLoop.cancel()
 
-proc getPeer*(query: DiscoveryQuery): Future[DiscoveryFilters] {.async.} =
+proc getPeer*(query: DiscoveryQuery): Future[PeerAttributes] {.async.} =
   let getter = query.peers.popFirst()
 
   try:
