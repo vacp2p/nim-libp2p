301a506c347753c9e42c6a160432f60641d0c9f3
Ludovic Chenut <ludovic@status.im>
2022-10-24 13:20:34 +0200

    tutorial v2

--- examples/tutorial_5_discovery.nim
+++ examples/tutorial_5_discovery.nim
@@ -3,15 +3,15 @@
 ## In the [previous tutorial](tutorial_4_gossipsub.md), we built a custom protocol using [protobuf](https://developers.google.com/protocol-buffers) and
 ## spread informations (some metrics) on the network using gossipsub.
 ## For this tutorial, on the other hand, we'll go back on a simple example
-## we'll try to discover a specific peer to ping on the network.
+## we'll try to discover a specific peers to greet on the network.
 ##
 ## First, as usual, we import the dependencies:
 import sequtils
 import chronos
+import stew/byteutils
 
 import libp2p
 import libp2p/protocols/rendezvous
-import libp2p/protocols/ping
 import libp2p/discovery/rendezvousinterface
 import libp2p/discovery/discoverymngr
 
@@ -31,69 +31,99 @@ proc createSwitch(rdv: RendezVous = RendezVous.new()): Switch =
     .withRendezVous(rdv)
     .build()
 
+# Create a really simple protocol to log one message received then close the connection
+const DumbCodec = "/dumb/proto/1.0.0"
+type DumbProto = ref object of LPProtocol
+proc new(T: typedesc[DumbProto], nodeNumber: int, fut: Future[void]): T =
+  proc handle(conn: Connection, proto: string) {.async, gcsafe.} =
+    echo "Node", nodeNumber, " received: ", string.fromBytes(await conn.readLp(1024))
+    await conn.close()
+    fut.complete()
+  return T(codecs: @[DumbCodec], handler: handle)
+
 ## We can create our main procedure:
 proc main() {.async, gcsafe.} =
-  # Create a "network" of six nodes looking like that:
-  # node0 <-> node1 <-> node2 <-> node3 <-> node4 <-> node5
+  # First of all, we'll create and start a boot node that'll be
+  # used as an information hub on the network
+  let bootNode = createSwitch()
+  await bootNode.start()
+
   var
     switches: seq[Switch] = @[]
     discManagers: seq[DiscoveryManager] = @[]
+    remoteFutures: seq[Future[void]] = @[]
+
   for i in 0..5:
     # Create a RendezVous Protocol
     let rdv = RendezVous.new()
+    # Create a remote future to await at the end of the program
+    let remoteFut = newFuture[void]()
+    remoteFutures.add(remoteFut)
     # Create a switch, mount the RendezVous protocol to it, start it and
-    # eventually connect it to the previous node in the sequence
+    # eventually connect it to the bootNode
     let switch = createSwitch(rdv)
-    # The first switch'll ping, so we mount the protocol accordingly
-    if i == 0: switch.mount(Ping.new())
+    switch.mount(DumbProto.new(i, remoteFut))
     await switch.start()
-    if i > 0:
-      await switches[^1].connect(switch.peerInfo.peerId, switch.peerInfo.addrs)
+    await switch.connect(bootNode.peerInfo.peerId, bootNode.peerInfo.addrs)
     switches.add(switch)
 
-    # A discovery manager is a simple tool, you setup it by adding discovery
+    # A discovery manager is a simple tool, you set it up by adding discovery
     # interfaces (such as RendezVousInterface) then you can use it to advertise
     # something on the network or to request something from it.
     let dm = DiscoveryManager()
     # A RendezVousInterface is a RendezVous protocol wrapped to be usable by the
     # DiscoveryManager. You can initialize a time to advertise/request (tta/ttr)
     # for advertise/request every x seconds.
-    dm.add(RendezVousInterface.new(rdv, ttr = 250.milliseconds))
+    dm.add(RendezVousInterface.new(rdv))
+    # Each nodes of the network'll advertise on some topics (EvenGang or OddClub)
+    dm.advertise(RdvNamespace(if i mod 2 == 0: "EvenGang" else: "OddClub"))
     discManagers.add(dm)
 
-  # The first node of the network advertise on someTopic
-  discManagers[0].advertise(RdvNamespace("someTopic"))
-  for i in 1..4:
-    # All the four "middle" nodes request informations on someTopic
-    # to make the message spread. With a time to request of 250ms, it should
-    # spread on the four nodes in a little over 1 second.
-    discard discManagers[i].request(RdvNamespace("someTopic"))
+  # We can now create the newcomer, this peer'll connect to the boot node
   let
-    # The ending node request on someTopic aswell and await for information.
-    query = discManagers[5].request(RdvNamespace("someTopic"))
-    # getPeer give you a PeerAttribute containing informations about the peer.
-    # The most useful are the PeerId and the MultiAddresses
-    res = await query.getPeer()
+    rdv = RendezVous.new()
+    newcomer = createSwitch(rdv)
+    dm = DiscoveryManager()
+  await newcomer.start()
+  await newcomer.connect(bootNode.peerInfo.peerId, bootNode.peerInfo.addrs)
+  dm.add(RendezVousInterface.new(rdv, ttr = 250.milliseconds))
 
-  # We must stop the query after that, otherwise, it will continue to receive data
-  query.stop()
+  # Use the discovery manager to find peers on the OddClub topic the greet them
+  let queryOddClub = dm.request(RdvNamespace("OddClub"))
+  for _ in 0..2:
+    let
+      # getPeer give you a PeerAttribute containing informations about the peer.
+      # The most useful are the PeerId and the MultiAddresses
+      res = await queryOddClub.getPeer()
+      # Open a connection to the peer found to greet him
+      conn = await newcomer.dial(res[PeerId], res.getAll(MultiAddress), DumbCodec)
+    await conn.writeLp("Odd Club suuuucks! Even Gang is better!")
+    # Uh-oh!
+    await conn.close()
+  queryOddClub.stop()
 
-  doAssert(res[PeerId] == switches[0].peerInfo.peerId)
-  let resMa = res.getAll(MultiAddress)
-  for ma in resMa:
-    doAssert(ma in switches[0].peerInfo.addrs)
-  echo "Peer Found: ", res[PeerId], " ", resMa
-  # The peer Id and the address of the node0 is discovered by the node5 after
-  # being pass by all the intermediate node.
+  # Maybe it was because he wanted to join the EvenGang
+  let queryEvenGang = dm.request(RdvNamespace("EvenGang"))
+  for _ in 0..2:
+    let
+      res = await queryEvenGang.getPeer()
+      conn = await newcomer.dial(res[PeerId], res.getAll(MultiAddress), DumbCodec)
+    await conn.writeLp("Even Gang is sooo laaame! Odd Club rocks!")
+    # Or maybe not...
+    await conn.close()
+  queryEvenGang.stop()
+  # What can I say, some people just want to watch the world burn... Anyway
 
-  # Now the node5 can connect and ping the node1
-  let conn = await switches[5].dial(res[PeerId], resMa, PingCodec)
-  echo "ping: ", await Ping.new().ping(conn)
+  # Wait for all the remote futures to end
+  await allFutures(remoteFutures)
 
-  # Stop all the DiscoveryManagers
-  for dm in discManagers: dm.stop()
+  # Stop all the discovery managers
+  for d in discManagers:
+    d.stop()
+  dm.stop()
 
   # Stop all the switches
   await allFutures(switches.mapIt(it.stop()))
+  await allFutures(bootNode.stop(), newcomer.stop())
 
 waitFor(main())
