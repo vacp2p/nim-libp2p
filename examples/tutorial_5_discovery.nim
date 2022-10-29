229f16bedda2fc17afb6c44cc0cd29ee8d7a90ab
Tanguy <tanguy@status.im>
2022-10-29 10:58:09 +0200

    Touchups

--- examples/tutorial_5_discovery.nim
+++ examples/tutorial_5_discovery.nim
@@ -18,69 +18,70 @@ import libp2p/discovery/discoverymngr
 ## We'll not use newStandardSwitch this time as we need the discovery protocol
 ## [RendezVous](https://github.com/libp2p/specs/blob/master/rendezvous/README.md) to be mounted on the switch using withRendezVous.
 ##
-## Note that other discovery methods such as [Kademlia](https://github.com/libp2p/specs/blob/master/kad-dht/README.md) or [discv5](https://github.com/ethereum/devp2p/blob/master/discv5/discv5.md) exist (not everyone
-## of those are implemented yet though), but to make it simple for this tutorial,
-## we'll stick to RendezVous.
+## Note that other discovery methods such as [Kademlia](https://github.com/libp2p/specs/blob/master/kad-dht/README.md) or [discv5](https://github.com/ethereum/devp2p/blob/master/discv5/discv5.md) exist.
 proc createSwitch(rdv: RendezVous = RendezVous.new()): Switch =
   SwitchBuilder.new()
     .withRng(newRng())
     .withAddresses(@[ MultiAddress.init("/ip4/0.0.0.0/tcp/0").tryGet() ])
     .withTcpTransport()
-    .withMplex()
+    .withYamux()
     .withNoise()
     .withRendezVous(rdv)
     .build()
 
-# Create a really simple protocol to log one message received then close the connection
+# Create a really simple protocol to log one message received then close the stream
 const DumbCodec = "/dumb/proto/1.0.0"
 type DumbProto = ref object of LPProtocol
-proc new(T: typedesc[DumbProto], nodeNumber: int, fut: Future[void]): T =
+proc new(T: typedesc[DumbProto], nodeNumber: int): T =
   proc handle(conn: Connection, proto: string) {.async, gcsafe.} =
-    if fut.done(): return
     echo "Node", nodeNumber, " received: ", string.fromBytes(await conn.readLp(1024))
     await conn.close()
-    fut.complete()
   return T(codecs: @[DumbCodec], handler: handle)
 
-## We can create our main procedure:
+## # Bootnodes
+## The first time a p2p program is ran, he needs to know how to join
+## its network. This is generally done by hard-coding a list of stable
+## nodes in the binary, called "bootnodes". These bootnodes are a
+## critical part of a p2p network, since they are used by every new
+## user to onboard the network.
+##
+## By using libp2p, we can use any node supporting our discovery protocol
+## (rendezvous in this case) as a bootnode. For this example, we'll
+## create a bootnode, and then every peer will advertise itself on the
+## bootnode, and use it to find other peers
 proc main() {.async, gcsafe.} =
-  # First of all, we'll create and start a boot node that'll be
-  # used as an information hub on the network
   let bootNode = createSwitch()
   await bootNode.start()
 
   var
     switches: seq[Switch] = @[]
     discManagers: seq[DiscoveryManager] = @[]
-    remoteFutures: seq[Future[void]] = @[]
 
+  # Create 5 nodes in the network
   for i in 0..5:
-    # Create a RendezVous Protocol
     let rdv = RendezVous.new()
     # Create a remote future to await at the end of the program
-    let remoteFut = newFuture[void]()
-    remoteFutures.add(remoteFut)
-    # Create a switch, mount the RendezVous protocol to it, start it and
-    # eventually connect it to the bootNode
     let switch = createSwitch(rdv)
-    switch.mount(DumbProto.new(i, remoteFut))
-    await switch.start()
-    await switch.connect(bootNode.peerInfo.peerId, bootNode.peerInfo.addrs)
+    switch.mount(DumbProto.new(i))
     switches.add(switch)
 
-    # A discovery manager is a simple tool, you set it up by adding discovery
+    # A discovery manager is a simple tool, you can set it up by adding discovery
     # interfaces (such as RendezVousInterface) then you can use it to advertise
     # something on the network or to request something from it.
     let dm = DiscoveryManager()
     # A RendezVousInterface is a RendezVous protocol wrapped to be usable by the
-    # DiscoveryManager. You can initialize a time to advertise/request (tta/ttr)
-    # for advertise/request every x seconds.
+    # DiscoveryManager.
     dm.add(RendezVousInterface.new(rdv))
-    # Each nodes of the network'll advertise on some topics (EvenGang or OddClub)
-    dm.advertise(RdvNamespace(if i mod 2 == 0: "EvenGang" else: "OddClub"))
     discManagers.add(dm)
 
-  # We can now create the newcomer, this peer'll connect to the boot node
+    # We can now start the switch and connect to the bootnode
+    await switch.start()
+    await switch.connect(bootNode.peerInfo.peerId, bootNode.peerInfo.addrs)
+
+    # Each nodes of the network will advertise on some topics (EvenGang or OddClub)
+    dm.advertise(RdvNamespace(if i mod 2 == 0: "EvenGang" else: "OddClub"))
+
+  # We can now create the newcomer, this peer will connect to the boot node
   let
     rdv = RendezVous.new()
     newcomer = createSwitch(rdv)
@@ -94,13 +95,15 @@ proc main() {.async, gcsafe.} =
   for _ in 0..2:
     let
       # getPeer give you a PeerAttribute containing informations about the peer.
-      # The most useful are the PeerId and the MultiAddresses
       res = await queryOddClub.getPeer()
-      # Open a connection to the peer found to greet him
+      # Here we will use the PeerId and the MultiAddress to greet him
       conn = await newcomer.dial(res[PeerId], res.getAll(MultiAddress), DumbCodec)
     await conn.writeLp("Odd Club suuuucks! Even Gang is better!")
     # Uh-oh!
     await conn.close()
+    # Wait for the peer to close the stream
+    await conn.join()
+  # Queries will run in a loop, so we must stop them when we are done
   queryOddClub.stop()
 
   # Maybe it was because he wanted to join the EvenGang
@@ -112,11 +115,9 @@ proc main() {.async, gcsafe.} =
     await conn.writeLp("Even Gang is sooo laaame! Odd Club rocks!")
     # Or maybe not...
     await conn.close()
+    await conn.join()
   queryEvenGang.stop()
   # What can I say, some people just want to watch the world burn... Anyway
-
-  # Wait for all the remote futures to end
-  await allFutures(remoteFutures)
 
   # Stop all the discovery managers
   for d in discManagers:
