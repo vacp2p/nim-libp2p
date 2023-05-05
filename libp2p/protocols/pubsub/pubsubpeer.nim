0f73ce0ea9392a5324cc98854c456ffa839b0d46
Tanguy <tanguy@status.im>
2023-05-05 15:09:53 +0200

    better

--- libp2p/protocols/pubsub/pubsubpeer.nim
+++ libp2p/protocols/pubsub/pubsubpeer.nim
@@ -174,7 +174,7 @@ proc connectOnce(p: PubSubPeer): Future[void] {.async.} =
   try:
     if p.connectedFut.finished:
       p.connectedFut = newFuture[void]()
-    let newConn = await p.getConn().withTimeout(5.seconds)
+    let newConn = await p.getConn().wait(5.seconds)
     if newConn.isNil:
       raise (ref LPError)(msg: "Cannot establish send connection")
 
@@ -200,6 +200,9 @@ proc connectOnce(p: PubSubPeer): Future[void] {.async.} =
       trace "Removing send connection", p, conn = p.sendConn
       await p.sendConn.close()
       p.sendConn = nil
+
+    if not p.connectedFut.finished:
+      p.connectedFut.complete()
 
     try:
       if p.onEvent != nil:
@@ -249,11 +252,13 @@ proc sendEncoded*(p: PubSubPeer, msg: seq[byte]) {.raises: [Defect], async.} =
     return
 
   if p.sendConn == nil:
-    discard await p.connectedFut.withTimeout(1.seconds)
+    # Wait for a send conn to be setup. `connectOnce` will
+    # complete this even if the sendConn setup failed
+    await p.connectedFut
 
   var conn = p.sendConn
   if conn == nil or conn.closed():
-    debug "No send connection, skipping message", p, msg = shortLog(msg)
+    debug "No send connection", p, msg = shortLog(msg)
     return
 
   trace "sending encoded msgs to peer", conn, encoded = shortLog(msg)
