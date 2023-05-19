cdefe85fd30ad93b83583bd2d5b89653608b15ad
Diego <diego@status.im>
2023-05-19 15:52:16 +0200

    more improvements

--- tests/testutility.nim
+++ tests/testutility.nim
@@ -17,58 +17,58 @@ suite "Utility":
 
   test "successful safeConvert from int8 to int16":
     let res = safeConvert[int16, int8]((-128).int8)
-    doAssert res == -128'i16, fmt"Expected -128 but got {result}"
+    check res == -128'i16
 
   test "unsuccessful safeConvert from int16 to int8":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: int8 = safeConvert[int8, int16](32767'i16))
 
   test "successful safeConvert from uint8 to uint16":
     let res: uint16 = safeConvert[uint16, uint8](255'u8)
-    doAssert res == 255'u16, fmt"Expected 255 but got {result}"
+    check res == 255'u16
 
   test "unsuccessful safeConvert from uint16 to uint8":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: uint8 = safeConvert[uint8, uint16](256'u16))
 
   test "successful safeConvert from char to int":
     let res: int = safeConvert[int, char]('A')
-    doAssert res == 65, fmt"Expected 65 but got {result}"
+    check res == 65
 
   test "unsuccessful safeConvert from int to char":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: char = safeConvert[char, int](128))
 
   test "successful safeConvert from bool to int":
     let res: int = safeConvert[int, bool](true)
-    doAssert res == 1, fmt"Expected 1 but got {result}"
+    check res == 1
 
   test "unsuccessful safeConvert from int to bool":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: bool = safeConvert[bool, int](2))
 
   test "successful safeConvert from enum to int":
     type Color = enum red, green, blue
     let res: int = safeConvert[int, Color](green)
-    doAssert res == 1, fmt"Expected 1 but got {result}"
+    check res == 1
 
   test "unsuccessful safeConvert from int to enum":
     type Color = enum red, green, blue
-    doAssert not (compiles do:
+    check not (compiles do:
       result: Color = safeConvert[Color, int](3))
 
   test "successful safeConvert from range to int":
     let res: int = safeConvert[int, range[1..10]](5)
-    doAssert res == 5, fmt"Expected 5 but got {result}"
+    check res == 5
 
   test "unsuccessful safeConvert from int to range":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: range[1..10] = safeConvert[range[1..10], int](11))
 
   test "unsuccessful safeConvert from int to uint":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: uint = safeConvert[uint, int](11))
 
   test "unsuccessful safeConvert from uint to int":
-    doAssert not (compiles do:
+    check not (compiles do:
       result: uint = safeConvert[int, uint](11.uint))
