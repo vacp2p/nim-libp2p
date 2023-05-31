67102873ba958a0ce92837ca86711b7219707f98
diegomrsantos <diego@status.im>
2023-05-31 08:59:50 +0200

    Fail only if all addresses in PeerRecord are invalid (#898)
    Fixes https://github.com/waku-org/nwaku/issues/1768

--- tests/testrouting_record.nim
+++ tests/testrouting_record.nim
@@ -77,3 +77,41 @@ suite "Signed Routing Record":
       buffer = routingRecord.envelope.encode().tryGet()
 
     check SignedPeerRecord.decode(buffer).error == EnvelopeInvalidSignature
+
+  test "Decode doesn't fail if some addresses are invalid":
+    let
+      rng = newRng()
+      privKey = PrivateKey.random(rng[]).tryGet()
+      peerId = PeerId.init(privKey).tryGet()
+      multiAddresses = @[MultiAddress(), MultiAddress.init("/ip4/0.0.0.0/tcp/25").tryGet()]
+      routingRecord = PeerRecord.init(peerId, multiAddresses, 42)
+
+      buffer = routingRecord.encode()
+      parsedRR = PeerRecord.decode(buffer).tryGet()
+
+    check parsedRR.addresses.len == 1
+
+  test "Decode doesn't fail if there are no addresses":
+    let
+      rng = newRng()
+      privKey = PrivateKey.random(rng[]).tryGet()
+      peerId = PeerId.init(privKey).tryGet()
+      multiAddresses = newSeq[MultiAddress]()
+      routingRecord = PeerRecord.init(peerId, multiAddresses, 42)
+
+      buffer = routingRecord.encode()
+      parsedRR = PeerRecord.decode(buffer).tryGet()
+
+    check parsedRR.addresses.len == 0
+
+  test "Decode fails if all addresses are invalid":
+    let
+      rng = newRng()
+      privKey = PrivateKey.random(rng[]).tryGet()
+      peerId = PeerId.init(privKey).tryGet()
+      multiAddresses = @[MultiAddress(), MultiAddress()]
+      routingRecord = PeerRecord.init(peerId, multiAddresses, 42)
+
+      buffer = routingRecord.encode()
+
+    check PeerRecord.decode(buffer).isErr
