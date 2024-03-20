3c74db539d6a8ab0810fa43493aef35d3368bf46
Jacek Sieka <jacek@status.im>
2024-03-20 06:57:16 +0100

    a few more tests

--- tests/pubsub/testtimedcache.nim
+++ tests/pubsub/testtimedcache.nim
@@ -24,6 +24,8 @@ suite "TimedCache":
       2 in cache
       3 in cache
 
+      cache.addedAt(2) == now + 3.seconds
+
     check:
       cache.put(2, now + 7.seconds) # refreshes 2
       not cache.put(4, now + 12.seconds) # expires 3
@@ -40,3 +42,16 @@ suite "TimedCache":
     check:
       not cache.put(100, now + 100.seconds) # expires everything
       100 in cache
+      2 notin cache
+
+  test "spam":
+    var cache = TimedCache[int].init(5.seconds)
+
+    let now = Moment.now()
+    for i in 101..100000:
+      check:
+        not cache.put(i, now)
+
+    for i in 101..100000:
+      check:
+        i in cache
