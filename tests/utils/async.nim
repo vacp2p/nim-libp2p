1ce7cc755fe7d836b393c4ba74e2c74d03828a46
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2024-08-23 18:42:13 +0200

    Update waitForResult to handle void futures.

--- tests/utils/async.nim
+++ tests/utils/async.nim
@@ -1,10 +1,19 @@
 import chronos/futures, stew/results, chronos
 
 proc waitForResult*[T](
-    fut: Future[T], timeout: Duration
+    fut: Future[T], timeout: Duration = 1.seconds
 ): Future[Result[T, string]] {.async.} =
   try:
     let val = await fut.wait(timeout)
     return Result[T, string].ok(val)
   except Exception as e:
     return Result[T, string].err(e.msg)
+
+proc waitForResult*(
+    fut: Future[void], timeout: Duration = 1.seconds
+): Future[Result[void, string]] {.async.} =
+  try:
+    await fut.wait(timeout)
+    return Result[void, string].ok()
+  except Exception as e:
+    return Result[void, string].err(e.msg)
