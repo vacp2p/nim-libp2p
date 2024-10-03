3290e64e3a4db7b221545d28b0f1521fa81cbcb2
Diego <diego@status.im>
2024-10-03 17:00:15 +0200

    feat: generate and parse libp2p tls certificate

---
+++ tests/transports/tls/testcertificate.nim
@@ -0,0 +1,21 @@
+import unittest2
+
+import ../../../libp2p/transports/tls/certificate
+import ../../../libp2p/crypto/crypto
+import ../../../libp2p/peerid
+
+suite "Certificate Tests":
+
+  test "sanity check":
+    var rng = newRng()
+
+    # Generate an Ed25519 keypair
+    let keypair = KeyPair.random(Secp256k1, rng[]).tryGet()
+    let peerId = PeerId.init(keypair.pubkey).tryGet()
+
+    let certBytes = generate(keypair)[0]
+    let cert = parse(certBytes)
+    let ext = cert.extension
+
+    let parsedPeerId = PeerId.init(PublicKey.init(ext.publicKey).tryGet).tryGet()
+    check parsedPeerId == peerId
