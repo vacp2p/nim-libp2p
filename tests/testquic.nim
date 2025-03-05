85a7f18c3ea02428c0d1cf6072ffdf471b8ec3a2
Richard Ramos <info@richardramos.me>
2025-03-05 15:53:14 -0400

    fix: tests

--- tests/testquic.nim
+++ tests/testquic.nim
@@ -23,9 +23,11 @@ suite "Quic transport":
     await transport1.start(ma)
     check transport1.handles(transport1.addrs[0])
     await transport1.stop()
-#
+  #
   asyncTest "transport e2e":
-    let serverMA = @[MultiAddress.init("/ip4/127.0.0.1/udp/50001/quic-v1").tryGet()] # todo: HANDLE 0 port
+    let serverMA =
+      @[MultiAddress.init("/ip4/127.0.0.1/udp/50001/quic-v1").tryGet()]
+        # todo: HANDLE 0 port
     let privateKey = PrivateKey.random(ECDSA, (newRng())[]).tryGet()
     let server: QuicTransport = QuicTransport.new(Upgrade(), privateKey)
     await server.start(serverMA)
@@ -34,24 +36,24 @@ suite "Quic transport":
       let privateKey = PrivateKey.random(ECDSA, (newRng())[]).tryGet()
       let client: QuicTransport = QuicTransport.new(Upgrade(), privateKey)
       let conn = await client.dial("", server.addrs[0])
-      await conn.write("client")
+      let stream = await getStream(QuicSession(conn), Direction.Out)
+      await stream.write("client")
       var resp: array[6, byte]
-      await conn.readExactly(addr resp, 6)
-      await conn.close()
-
+      await stream.readExactly(addr resp, 6)
+      await stream.close()
       check string.fromBytes(resp) == "server"
       await client.stop()
 
     proc serverAcceptHandler() {.async.} =
       let conn = await server.accept()
+      let stream = await getStream(QuicSession(conn), Direction.In)
       var resp: array[6, byte]
-      await conn.readExactly(addr resp, 6)
+      await stream.readExactly(addr resp, 6)
       check string.fromBytes(resp) == "client"
 
-      await conn.write("server")
-      await conn.close()
+      await stream.write("server")
+      await stream.close()
       await server.stop()
-
 
     asyncSpawn serverAcceptHandler()
     await runClient()
