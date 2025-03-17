17e818cd3461a2875c4cf3b9703d1ec5309635cf
Vlado Pajić <vlado@brrrrr.at>
2025-03-17 16:29:18 +0100

    fix test

--- tests/transports/tls/testcertificate.nim
+++ tests/transports/tls/testcertificate.nim
@@ -5,16 +5,15 @@ import ../../../libp2p/crypto/crypto
 import ../../../libp2p/peerid
 
 suite "Certificate Tests":
-  test "sanity check":
-    var rng = newRng()
+  test "Generate with DER ecoding":    
+    let schemes = @[Ed25519, Secp256k1, ECDSA]
+    for scheme in schemes:
+      var rng = newRng()
+      let keypair = KeyPair.random(scheme, rng[]).tryGet()
+      let peerId = PeerId.init(keypair.pubkey).tryGet()
 
-    # Generate an Ed25519 keypair
-    let keypair = KeyPair.random(Secp256k1, rng[]).tryGet()
-    let peerId = PeerId.init(keypair.pubkey).tryGet()
+      let (certBytes, _) = generate(keypair, EncodingFormat.DER)
+      let cert = parse(certBytes)
 
-    let (certBytes, _) = generate(keypair, EncodingFormat.DER)
-    let cert = parse(certBytes)
-    let ext = cert.extension
-
-    let parsedPeerId = PeerId.init(PublicKey.init(ext.publicKey).tryGet).tryGet()
-    check parsedPeerId == peerId
+      let parsedPeerId = PeerId.init(PublicKey.init(cert.extension.publicKey).tryGet).tryGet()
+      check parsedPeerId == peerId
