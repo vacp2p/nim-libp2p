93659dec396bf5ca0fb33d9d74ff8cb92da1900e
Radoslaw Kaminski <radoslaw@status.im>
2025-04-25 10:38:06 +0100

    test: add testfanout

---
+++ tests/pubsub/testfanout.nim
@@ -0,0 +1,113 @@
+# Nim-LibP2P
+# Copyright (c) 2023-2024 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+{.used.}
+
+import std/[sequtils]
+import utils
+import ../../libp2p/protocols/pubsub/[gossipsub]
+import ../helpers
+
+suite "GossipSub internal":
+  teardown:
+    checkTrackers()
+
+  asyncTest "`replenishFanout` Degree Lo":
+    let gossipSub = TestGossipSub.init(newStandardSwitch())
+
+    proc handler(peer: PubSubPeer, data: seq[byte]) {.async: (raises: []).} =
+      discard
+
+    let topic = "foobar"
+    gossipSub.gossipsub[topic] = initHashSet[PubSubPeer]()
+    gossipSub.topicParams[topic] = TopicParams.init()
+
+    var conns = newSeq[Connection]()
+    for i in 0 ..< 15:
+      let conn = TestBufferStream.new(noop)
+      conns &= conn
+      var peerId = randomPeerId()
+      conn.peerId = peerId
+      let peer = gossipSub.getPubSubPeer(peerId)
+      peer.handler = handler
+      gossipSub.gossipsub[topic].incl(peer)
+
+    check gossipSub.gossipsub[topic].len == 15
+    gossipSub.replenishFanout(topic)
+    check gossipSub.fanout[topic].len == gossipSub.parameters.d
+
+    await allFuturesThrowing(conns.mapIt(it.close()))
+    await gossipSub.switch.stop()
+
+  asyncTest "`dropFanoutPeers` drop expired fanout topics":
+    let gossipSub = TestGossipSub.init(newStandardSwitch())
+
+    proc handler(peer: PubSubPeer, data: seq[byte]) {.async: (raises: []).} =
+      discard
+
+    let topic = "foobar"
+    gossipSub.topicParams[topic] = TopicParams.init()
+    gossipSub.fanout[topic] = initHashSet[PubSubPeer]()
+    gossipSub.lastFanoutPubSub[topic] = Moment.fromNow(1.millis)
+    await sleepAsync(5.millis) # allow the topic to expire
+
+    var conns = newSeq[Connection]()
+    for i in 0 ..< 6:
+      let conn = TestBufferStream.new(noop)
+      conns &= conn
+      let peerId = PeerId.init(PrivateKey.random(ECDSA, rng[]).get()).tryGet()
+      conn.peerId = peerId
+      let peer = gossipSub.getPubSubPeer(peerId)
+      peer.handler = handler
+      gossipSub.fanout[topic].incl(peer)
+
+    check gossipSub.fanout[topic].len == gossipSub.parameters.d
+
+    gossipSub.dropFanoutPeers()
+    check topic notin gossipSub.fanout
+
+    await allFuturesThrowing(conns.mapIt(it.close()))
+    await gossipSub.switch.stop()
+
+  asyncTest "`dropFanoutPeers` leave unexpired fanout topics":
+    let gossipSub = TestGossipSub.init(newStandardSwitch())
+
+    proc handler(peer: PubSubPeer, data: seq[byte]) {.async: (raises: []).} =
+      discard
+
+    let topic1 = "foobar1"
+    let topic2 = "foobar2"
+    gossipSub.topicParams[topic1] = TopicParams.init()
+    gossipSub.topicParams[topic2] = TopicParams.init()
+    gossipSub.fanout[topic1] = initHashSet[PubSubPeer]()
+    gossipSub.fanout[topic2] = initHashSet[PubSubPeer]()
+    gossipSub.lastFanoutPubSub[topic1] = Moment.fromNow(1.millis)
+    gossipSub.lastFanoutPubSub[topic2] = Moment.fromNow(1.minutes)
+    await sleepAsync(5.millis) # allow the topic to expire
+
+    var conns = newSeq[Connection]()
+    for i in 0 ..< 6:
+      let conn = TestBufferStream.new(noop)
+      conns &= conn
+      let peerId = randomPeerId()
+      conn.peerId = peerId
+      let peer = gossipSub.getPubSubPeer(peerId)
+      peer.handler = handler
+      gossipSub.fanout[topic1].incl(peer)
+      gossipSub.fanout[topic2].incl(peer)
+
+    check gossipSub.fanout[topic1].len == gossipSub.parameters.d
+    check gossipSub.fanout[topic2].len == gossipSub.parameters.d
+
+    gossipSub.dropFanoutPeers()
+    check topic1 notin gossipSub.fanout
+    check topic2 in gossipSub.fanout
+
+    await allFuturesThrowing(conns.mapIt(it.close()))
+    await gossipSub.switch.stop()
