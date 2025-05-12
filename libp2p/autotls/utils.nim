0edd96c6426a82e4ae366bce1daf0fb9d31e0f03
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-05-12 11:40:59 -0300

    feat(autotls): peerID authentication delivery to autoTLS broker

---
+++ libp2p/autotls/utils.nim
@@ -0,0 +1,62 @@
+import base64, strutils, stew/base36, chronos/apps/http/httpclient, json, strformat
+import
+  ../errors, ../peerid, ../multihash, ../cid, ../multicodec, ../crypto/[crypto, rsa]
+
+type AutoTLSError* = object of LPError
+type ACMEError* = object of AutoTLSError
+type PeerIDAuthError* = object of AutoTLSError
+
+const
+  AutoTLSBroker* = "https://registration.libp2p.direct"
+  AutoTLSDNSServer* = "libp2p.direct"
+
+proc base64UrlEncode*(data: seq[byte]): string =
+  ## Encodes data using base64url (RFC 4648 §5) — no padding, URL-safe
+  var encoded = base64.encode(data, safe = true)
+  encoded.removeSuffix("=")
+  encoded.removeSuffix("=")
+  return encoded
+
+proc encodePeerId*(peerId: PeerId): string {.raises: [AutoTLSError].} =
+  var mh: MultiHash
+  let decodeResult = MultiHash.decode(peerId.data, mh)
+  if decodeResult.isErr or decodeResult.get() == -1:
+    raise
+      newException(AutoTLSError, "Failed to decode PeerId: invalid multihash format")
+
+  let cidResult = Cid.init(CIDv1, multiCodec("libp2p-key"), mh)
+  if cidResult.isErr:
+    raise newException(AutoTLSError, "Failed to initialize CID from multihash")
+
+  return Base36.encode(cidResult.get().data.buffer)
+
+proc getParsedResponseBody*(
+    response: HttpClientResponseRef
+): Future[JsonNode] {.async: (raises: [ACMEError]).} =
+  try:
+    let responseBody = bytesToString(await response.getBodyBytes()).parseJson()
+    return responseBody
+  except ValueError, OSError, IOError:
+    raise newException(ACMEError, "Unable to parse JSON body")
+  except Exception as e:
+    raise newException(
+      ACMEError, "Unexpected error occurred while getting body bytes: " & e.msg
+    )
+
+proc getJSONField*(node: JsonNode, field: string): JsonNode {.raises: [ACMEError].} =
+  if node{field} == nil:
+    raise newException(ACMEError, fmt"'{field}' field not found in JSON: " & $node)
+  return node{field}
+
+proc thumbprint*(key: KeyPair): string =
+  # TODO: check if scheme is RSA
+  let pubkey = key.pubkey.rsakey
+  let nArray = @(getArray(pubkey.buffer, pubkey.key.n, pubkey.key.nlen))
+  let eArray = @(getArray(pubkey.buffer, pubkey.key.e, pubkey.key.elen))
+
+  let n = base64UrlEncode(nArray)
+  let e = base64UrlEncode(eArray)
+  let keyJson = %*{"e": e, "kty": "RSA", "n": n}
+  # TODO: https://www.rfc-editor.org/rfc/rfc7638
+  let digest = sha256.digest($keyJson)
+  return base64UrlEncode(@(digest.data))
