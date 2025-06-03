0634a018c92a83c3fc154d61aab2f6ab93b511eb
Radoslaw Kaminski <radoslaw@status.im>
2025-06-03 11:18:10 +0100

    test: extend testhelpers

--- tests/testhelpers.nim
+++ tests/testhelpers.nim
@@ -10,8 +10,10 @@
 # those terms.
 
 import ./helpers
+import unittest2
+from std/exitprocs import nil
 
-suite "Helpers":
+suite "checkUntilTimeout helpers":
   asyncTest "checkUntilTimeout should pass if the condition is true":
     let a = 2
     let b = 2
@@ -26,6 +28,17 @@ suite "Helpers":
       a == 2
       b == 2
 
+  asyncTest "checkUntilTimeout should pass if condition becomes true after time":
+    var a = 1
+    let b = 2
+    proc makeConditionTrueLater() {.async.} =
+      await sleepAsync(100.milliseconds)
+      a = 2
+
+    asyncSpawn makeConditionTrueLater()
+    checkUntilTimeout:
+      a == b
+
   asyncTest "checkUntilCustomTimeout should pass when the condition is true":
     let a = 2
     let b = 2
@@ -39,3 +52,32 @@ suite "Helpers":
       a == b
       a == 2
       b == 2
+
+  asyncTest "checkUntilCustomTimeout should pass if condition becomes true after time":
+    var a = 1
+    let b = 2
+    proc makeConditionTrueLater() {.async.} =
+      await sleepAsync(100.milliseconds)
+      a = 2
+
+    asyncSpawn makeConditionTrueLater()
+    checkUntilCustomTimeout(200.milliseconds, 10.milliseconds):
+      a == b
+
+suite "checkUntilTimeout helpers - failed":
+  teardown:
+    require testStatusIMPL == TestStatus.Failed
+    testStatusIMPL = TestStatus.OK
+    exitProcs.setProgramResult(QuitSuccess)
+
+  asyncTest "checkUntilTimeout should timeout if condition is never true":
+    let a = 1
+    let b = 2
+    checkUntilTimeout:
+      a == b
+
+  asyncTest "checkUntilCustomTimeout should timeout if condition is never true":
+    let a = 1
+    let b = 2
+    checkUntilCustomTimeout(100.milliseconds, 10.milliseconds):
+      a == b
