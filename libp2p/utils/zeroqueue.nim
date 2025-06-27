5958a2a8462cc4aaae9201da75bb675aa75ae9f0
vlado <vlado@brrrrr.at>
2025-06-27 11:40:23 +0200

    utilize deques

--- libp2p/utils/zeroqueue.nim
+++ libp2p/utils/zeroqueue.nim
@@ -7,6 +7,8 @@
 # This file may not be copied, modified, or distributed except according to
 # those terms.
 
+import std/deques
+
 type Chunk = object
   data: seq[byte]
   size: int
@@ -22,38 +24,38 @@ type ZeroQueue* = object
   # ZeroQueue is queue structure optimized for efficient pushing and popping of 
   # byte sequences `seq[byte]` (called chunks). This type is useful for streaming or buffering 
   # scenarios where chunks of binary data are accumulated and consumed incrementally.
-  chunks: seq[Chunk]
+  chunks: Deque[Chunk]
 
 proc clear*(q: var ZeroQueue) =
-  q.chunks = @[]
+  q.chunks.clear()
 
 proc isEmpty*(q: ZeroQueue): bool =
-  return q.chunks.len == 0
+  return q.chunks.len() == 0
 
 proc len*(q: ZeroQueue): int64 =
   var l: int64
-  for b in q.chunks:
+  for b in q.chunks.items():
     l += b.len()
   return l
 
 proc push*(q: var ZeroQueue, b: sink seq[byte]) =
   if b.len > 0:
-    q.chunks.add(newChunk(b))
+    q.chunks.addLast(newChunk(b))
 
 proc popChunk(q: var ZeroQueue, count: int): Chunk {.inline.} =
-  let first = q.chunks[0]
+  var first = q.chunks.popFirst()
 
-  # first frame has up to requested count elements,
-  # queue will return this frame (frame might have less then requested)
+  # first chunk has up to requested count elements,
+  # queue will return this chunk (chunk might have less then requested)
   if first.len() <= count:
-    q.chunks = q.chunks[1 ..^ 1]
     return first
 
-  # first frame has more elements then requested count, 
+  # first chunk has more elements then requested count, 
   # queue will return view of first count elements, leaving the rest in the queue
   var ret = first
-  q.chunks[0].start += count
   ret.size = ret.start + count
+  first.start += count
+  q.chunks.addFirst(first)
   return ret
 
 proc consumeTo*(q: var ZeroQueue, pbytes: pointer, nbytes: int): int =
