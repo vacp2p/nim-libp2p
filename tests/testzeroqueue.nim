4e54dc8698cf825b23656f9fe75db2ec41a53059
vlado <vlado@brrrrr.at>
2025-06-27 10:10:45 +0200

    test cosmetics

--- tests/testzeroqueue.nim
+++ tests/testzeroqueue.nim
@@ -23,23 +23,30 @@ suite "ZeroQueue":
     var q: ZeroQueue
     check q.len() == 0
     check q.isEmpty()
-    check q.pop(1).len == 0
+    check q.popSeq(1).len == 0 # pop empty seq when queue is empty
 
     q.push(@[1'u8, 2, 3])
     q.push(@[4'u8, 5])
     check q.len() == 5
     check not q.isEmpty()
 
-    check q.pop(0).len == 0
-    check @[1'u8, 2, 3] == q.pop(3) # pop eactly the size
-    check @[4'u8] == q.pop(1) # pop less the pushed
-    check @[5'u8] == q.pop(5) # pop more then pushed
+    check q.popSeq(3) == @[1'u8, 2, 3] # pop eactly the size of the chunk
+    check q.popSeq(1) == @[4'u8] # pop less then size of the chunk
+    check q.popSeq(5) == @[5'u8] # pop more then size of the chunk
     check q.isEmpty()
 
     # should not push empty seq
     q.push(@[])
     q.push(@[])
     check q.isEmpty()
+
+  test "clear":
+    var q: ZeroQueue
+    q.push(@[1'u8, 2, 3])
+    check not q.isEmpty()
+    q.clear()
+    check q.isEmpty()
+    check q.len() == 0
 
   test "consumeTo":
     var q: ZeroQueue
@@ -49,25 +56,31 @@ suite "ZeroQueue":
       dealloc(pbytes)
 
     # consumeTo should fill up to nbytes (queue is emptied)
-    q.clear()
     q.push(@[1'u8, 2, 3])
     q.push(@[4'u8, 5])
     q.push(@[6'u8, 7])
     check q.consumeTo(pbytes, nbytes) == 7
-    check q.isEmpty()
     check toSeq(pbytes, 7) == @[1'u8, 2, 3, 4, 5, 6, 7]
+    check q.isEmpty()
 
     # consumeTo should fill only 1 element, leaving 2 elements in the queue
-    q.clear()
     q.push(@[1'u8, 2, 3])
     check q.consumeTo(pbytes, 1) == 1
-    check not q.isEmpty()
     check toSeq(pbytes, 1) == @[1'u8]
+    check q.len() == 2
+    # assert remaning elements
+    check q.consumeTo(pbytes, nbytes) == 2
+    check toSeq(pbytes, 2) == @[2'u8, 3]
+    check q.isEmpty()
 
     # consumeTo should fill only 3 element, leaving 2 elements in the queue
     q.clear()
     q.push(@[4'u8, 5])
     q.push(@[1'u8, 2, 3])
     check q.consumeTo(pbytes, 3) == 3
-    check not q.isEmpty()
     check toSeq(pbytes, 3) == @[4'u8, 5, 1]
+    check q.len() == 2
+    # assert remaning elements
+    check q.consumeTo(pbytes, nbytes) == 2
+    check toSeq(pbytes, 2) == @[2'u8, 3]
+    check q.isEmpty()
