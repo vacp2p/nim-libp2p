8f54367e3af496cd9d33edaaeef68dd382e17a75
Radosław Kamiński <radoslaw@status.im>
2025-07-01 16:19:44 +0100

    test(gossipsub): refactor compatibility related tests (#1495)

--- tests/pubsub/integration/testpubsubintegration.nim
+++ tests/pubsub/integration/testpubsubintegration.nim
@@ -2,6 +2,6 @@
 
 import
   testfloodsub, testgossipsubcontrolmessages, testgossipsubcustomconn,
-  testgossipsubfanout, testgossipsubgossip, testgossipsubheartbeat,
-  testgossipsubmeshmanagement, testgossipsubmessagecache, testgossipsubmessagehandling,
-  testgossipsubscoring
+  testgossipsubfanout, testgossipsubgossip, testgossipsubgossipcompatibility,
+  testgossipsubheartbeat, testgossipsubmeshmanagement, testgossipsubmessagecache,
+  testgossipsubmessagehandling, testgossipsubscoring
