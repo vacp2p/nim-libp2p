7aea2b666e8f71776ce4c273a639dbd619dfe81e
vlado <vlado@brrrrr.at>
2025-07-04 17:11:47 +0200

    add info to error

--- libp2p/switch.nim
+++ libp2p/switch.nim
@@ -358,7 +358,7 @@ proc start*(s: Switch) {.public, async: (raises: [CancelledError, LPError]).} =
   for fut in startFuts:
     if fut.failed:
       await s.stop()
-      raise newException(LPError, "starting transports failed", fut.error)
+      raise newException(LPError, "starting transports failed: " & $fut.errors.msg, fut.error)
 
   for t in s.transports: # for each transport
     if t.addrs.len > 0 or t.running:
