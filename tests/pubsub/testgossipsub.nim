9b33cea2253bc3470126b1bff7ed58398320d97c
Radosław Kamiński <radoslaw@status.im>
2025-07-08 07:46:28 +0100

    test(gossipsub): gossipsub unit tests 1 (#1508)
    Co-authored-by: vladopajic <vladopajic@users.noreply.github.com>

--- tests/pubsub/testgossipsub.nim
+++ tests/pubsub/testgossipsub.nim
@@ -14,6 +14,7 @@ import stew/byteutils
 import utils
 import ../../libp2p/protocols/pubsub/[gossipsub, mcache, peertable, pubsubpeer]
 import ../../libp2p/protocols/pubsub/rpc/[message, protobuf]
+import ../../libp2p/muxers/muxer
 import ../helpers
 
 suite "GossipSub":
@@ -21,6 +22,254 @@ suite "GossipSub":
 
   teardown:
     checkTrackers()
+
+  asyncTest "onNewPeer - sets peer stats and budgets and disconnects if bad score":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(1, topic)
+      peer = peers[0]
+      peerStats = PeerStats(
+        score: gossipSub.parameters.graylistThreshold - 1.0,
+        appScore: 10.0,
+        behaviourPenalty: 5.0,
+      )
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And existing peer stats are set
+    gossipSub.peerStats[peer.peerId] = peerStats
+
+    # And the peer is connected
+    gossipSub.switch.connManager.storeMuxer(Muxer(connection: conns[0]))
+    check:
+      gossipSub.switch.isConnected(peer.peerId)
+
+    # When onNewPeer is called
+    gossipSub.parameters.disconnectBadPeers = true
+    gossipSub.onNewPeer(peer)
+
+    # Then peer stats are updated
+    check:
+      peer.score == peerStats.score
+      peer.appScore == peerStats.appScore
+      peer.behaviourPenalty == peerStats.behaviourPenalty
+
+    # And peer budgets are set to default values
+    check:
+      peer.iHaveBudget == IHavePeerBudget
+      peer.pingBudget == PingsPeerBudget
+
+    # And peer is disconnected because score < graylistThreshold
+    checkUntilTimeout:
+      not gossipSub.switch.isConnected(peer.peerId)
+
+  asyncTest "onPubSubPeerEvent - StreamClosed removes peer from mesh and fanout":
+    # Given a GossipSub instance with one peer in both mesh and fanout
+    let
+      (gossipSub, conns, peers) =
+        setupGossipSubWithPeers(1, topic, populateMesh = true, populateFanout = true)
+      peer = peers[0]
+      event = PubSubPeerEvent(kind: PubSubPeerEventKind.StreamClosed)
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    check:
+      gossipSub.mesh.hasPeerId(topic, peer.peerId)
+      gossipSub.fanout.hasPeerId(topic, peer.peerId)
+
+    # When StreamClosed event is handled
+    gossipSub.onPubSubPeerEvent(peer, event)
+
+    # Then peer is removed from both mesh and fanout
+    check:
+      not gossipSub.mesh.hasPeerId(topic, peer.peerId)
+      not gossipSub.fanout.hasPeerId(topic, peer.peerId)
+
+  asyncTest "onPubSubPeerEvent - DisconnectionRequested disconnects peer":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(
+        1, topic, populateGossipsub = true, populateMesh = true, populateFanout = true
+      )
+      peer = peers[0]
+      event = PubSubPeerEvent(kind: PubSubPeerEventKind.DisconnectionRequested)
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And the peer is connected
+    gossipSub.switch.connManager.storeMuxer(Muxer(connection: conns[0]))
+    check:
+      gossipSub.switch.isConnected(peer.peerId)
+      gossipSub.mesh.hasPeerId(topic, peer.peerId)
+      gossipSub.fanout.hasPeerId(topic, peer.peerId)
+      gossipSub.gossipsub.hasPeerId(topic, peer.peerId)
+
+    # When DisconnectionRequested event is handled
+    gossipSub.onPubSubPeerEvent(peer, event)
+
+    # Then peer should be disconnected
+    checkUntilTimeout:
+      not gossipSub.switch.isConnected(peer.peerId)
+      not gossipSub.mesh.hasPeerId(topic, peer.peerId)
+      not gossipSub.fanout.hasPeerId(topic, peer.peerId)
+      not gossipSub.gossipsub.hasPeerId(topic, peer.peerId)
+
+  asyncTest "unsubscribePeer - handles nil peer gracefully":
+    # Given a GossipSub instance
+    let (gossipSub, conns, peers) = setupGossipSubWithPeers(0, topic)
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And a non-existent peer ID
+    let nonExistentPeerId = randomPeerId()
+
+    # When unsubscribePeer is called with non-existent peer
+    gossipSub.unsubscribePeer(nonExistentPeerId)
+
+    # Then no errors occur (method returns early for nil peers)
+    check:
+      true
+
+  asyncTest "unsubscribePeer - removes peer from mesh, gossipsub, fanout and subscribedDirectPeers":
+    # Given a GossipSub instance with one peer in mesh
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(
+        1, topic, populateGossipsub = true, populateMesh = true, populateFanout = true
+      )
+      peer = peers[0]
+      peerId = peer.peerId
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And peer is configured as a direct peer
+    gossipSub.parameters.directPeers[peerId] = @[]
+    discard gossipSub.subscribedDirectPeers.addPeer(topic, peer)
+
+    check:
+      gossipSub.mesh.hasPeerId(topic, peerId)
+      gossipSub.gossipsub.hasPeerId(topic, peerId)
+      gossipSub.fanout.hasPeerId(topic, peerId)
+      gossipSub.subscribedDirectPeers.hasPeerId(topic, peerId)
+
+    # When unsubscribePeer is called
+    gossipSub.unsubscribePeer(peerId)
+
+    # Then peer is removed from mesh
+    check:
+      not gossipSub.mesh.hasPeerId(topic, peerId)
+      not gossipSub.gossipsub.hasPeerId(topic, peerId)
+      not gossipSub.fanout.hasPeerId(topic, peerId)
+      not gossipSub.subscribedDirectPeers.hasPeerId(topic, peerId)
+
+  asyncTest "unsubscribePeer - resets firstMessageDeliveries in peerStats":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(1, topic)
+      peer = peers[0]
+      peerId = peer.peerId
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And peer stats with firstMessageDeliveries set
+    gossipSub.peerStats[peerId] = PeerStats()
+    gossipSub.peerStats[peerId].topicInfos[topic] =
+      TopicInfo(firstMessageDeliveries: 5.0)
+    check:
+      gossipSub.peerStats[peerId].topicInfos[topic].firstMessageDeliveries == 5.0
+
+    # When unsubscribePeer is called
+    gossipSub.unsubscribePeer(peerId)
+
+    # Then firstMessageDeliveries is reset to 0
+    gossipSub.peerStats.withValue(peerId, stats):
+      check:
+        stats[].topicInfos[topic].firstMessageDeliveries == 0.0
+
+  asyncTest "unsubscribePeer - removes peer from peersInIP":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(1, topic)
+      peer = peers[0]
+      peerId = peer.peerId
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And peer has an address and is in peersInIP
+    let testAddress = MultiAddress.init("/ip4/127.0.0.1/tcp/0").tryGet()
+    peer.address = some(testAddress)
+    gossipSub.peersInIP[testAddress] = initHashSet[PeerId]()
+    gossipSub.peersInIP[testAddress].incl(peerId)
+
+    # And verify peer is initially in peersInIP
+    check:
+      peerId in gossipSub.peersInIP[testAddress]
+
+    # When unsubscribePeer is called
+    gossipSub.unsubscribePeer(peerId)
+
+    # Then peer is removed from peersInIP
+    check:
+      testAddress notin gossipSub.peersInIP
+
+  asyncTest "handleSubscribe via rpcHandler - subscribe and unsubscribe with direct peer":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(1, topic)
+      peer = peers[0]
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And the peer is configured as a direct peer
+    gossipSub.parameters.directPeers[peer.peerId] = @[]
+
+    # When a subscribe message is sent via RPC handler
+    await gossipSub.rpcHandler(
+      peer, encodeRpcMsg(RPCMsg.withSubs(@[topic], true), false)
+    )
+
+    # Then the peer is added to gossipsub for the topic
+    # And the peer is added to subscribedDirectPeers
+    check:
+      gossipSub.gossipsub.hasPeer(topic, peer)
+      gossipSub.subscribedDirectPeers.hasPeer(topic, peer)
+
+    # When Peer is added to the mesh and fanout
+    discard gossipSub.mesh.addPeer(topic, peer)
+    discard gossipSub.fanout.addPeer(topic, peer)
+
+    # And an unsubscribe message is sent via RPC handler
+    await gossipSub.rpcHandler(
+      peer, encodeRpcMsg(RPCMsg.withSubs(@[topic], false), false)
+    )
+
+    # Then the peer is removed from gossipsub, mesh and fanout
+    # And the peer is removed from subscribedDirectPeers
+    check:
+      not gossipSub.gossipsub.hasPeer(topic, peer)
+      not gossipSub.mesh.hasPeer(topic, peer)
+      not gossipSub.fanout.hasPeer(topic, peer)
+      not gossipSub.subscribedDirectPeers.hasPeer(topic, peer)
+
+  asyncTest "handleSubscribe via rpcHandler - subscribe unknown peer":
+    # Given a GossipSub instance with one peer
+    let
+      (gossipSub, conns, peers) = setupGossipSubWithPeers(1, topic)
+      peer = peers[0]
+    defer:
+      await teardownGossipSub(gossipSub, conns)
+
+    # And peer is not in gossipSub.peers
+    let nonExistentPeerId = randomPeerId()
+    peer.peerId = nonExistentPeerId # override PeerId
+
+    # When a subscribe message is sent via RPC handler
+    await gossipSub.rpcHandler(
+      peer, encodeRpcMsg(RPCMsg.withSubs(@[topic], true), false)
+    )
+
+    # Then the peer is ignored
+    check:
+      not gossipSub.gossipsub.hasPeer(topic, peer)
 
   asyncTest "subscribe/unsubscribeAll":
     let (gossipSub, conns, peers) =
