44bd89e9cabe0184fe29f9badbe28714eb21b6c3
Radoslaw Kaminski <radoslaw@status.im>
2025-07-14 17:25:12 +0100

    test: refactor

--- performance/main.nim
+++ performance/main.nim
@@ -1,81 +1,74 @@
-import stew/endians2, stew/byteutils, tables, strutils, os
-import ../libp2p, ../libp2p/protocols/pubsub/rpc/messages
-import ../libp2p/muxers/mplex/lpchannel, ../libp2p/protocols/ping
+import stew/endians2, stew/byteutils
+import tables, strutils, os
+import sequtils, hashes
+import metrics, metrics/chronos_httpserver
 import chronos
-import sequtils, hashes, metrics, metrics/chronos_httpserver
-from times import getTime, toUnix, fromUnix, `-`, initTime, `$`, inMilliseconds
+import ../libp2p
+import ../libp2p/protocols/pubsub/rpc/messages
+import ../libp2p/muxers/mplex/lpchannel
+import ../libp2p/protocols/ping
+from times import getTime, toUnix, fromUnix, `-`, initTime, `$`, inMilliseconds, nanosecond
 from nativesockets import getHostname
 
-const chunks = 1
+const
+  chunks = 1
 
 proc msgIdProvider(m: Message): Result[MessageId, ValidationResult] =
-  return ok(($m.data.hash).toBytes())
+  ok(($m.data.hash).toBytes())
 
-proc startMetricsServer(
-    serverIp: IpAddress, serverPort: Port
-): Result[MetricsHttpServerRef, string] =
+proc startMetricsServer(serverIp: IpAddress, serverPort: Port): Result[MetricsHttpServerRef, string] =
   info "Starting metrics HTTP server", serverIp = $serverIp, serverPort = $serverPort
-
   let metricsServerRes = MetricsHttpServerRef.new($serverIp, serverPort)
   if metricsServerRes.isErr():
     return err("metrics HTTP server start failed: " & $metricsServerRes.error)
-
   let server = metricsServerRes.value
   try:
     waitFor server.start()
   except CatchableError:
     return err("metrics HTTP server start failed: " & getCurrentExceptionMsg())
-
   info "Metrics HTTP server started", serverIp = $serverIp, serverPort = $serverPort
   ok(metricsServerRes.value)
 
 proc main {.async.} =
-  let
-    hostname = getHostname()
-    myId = parseInt(getEnv("PEERNUMBER"))
-    msg_rate = parseInt(getEnv("MSGRATE"))
-    msg_size = parseInt(getEnv("MSGSIZE"))
+  # --- Configuration ---
+  let hostname = getHostname()
+  let myId = parseInt(getEnv("PEERNUMBER"))
+  let msgRate = parseInt(getEnv("MSGRATE"))
+  let msgSize = parseInt(getEnv("MSGSIZE"))
+  let publisherCount = parseInt(getEnv("PEERS"))
+  let isPublisher = myId <= publisherCount
+  let rng = libp2p.newRng()
 
-    #publisherCount = client.param(int, "publisher_count")
-    publisherCount = parseInt(getEnv("PEERS"))
-    isPublisher = myId <= publisherCount
-    #isAttacker = (not isPublisher) and myId - publisherCount <= client.param(int, "attacker_count")
-    isAttacker = false
-    rng = libp2p.newRng()
-    #randCountry = rng.rand(distribCumSummed[^1])
-    #country = distribCumSummed.find(distribCumSummed.filterIt(it >= randCountry)[0])
   echo "Hostname: ", hostname
-  let
-    myport = 5000 + parseInt(getEnv("PEERNUMBER"))
-    myaddress = "0.0.0.0:" & $myport
-    address = initTAddress(myaddress)
-    switch =
-      SwitchBuilder
-        .new()
-        .withAddress(MultiAddress.init(address).tryGet())
-        .withRng(rng)
-        #.withYamux()
-        .withMplex()
-        .withMaxConnections(250)
-        .withTcpTransport(flags = {ServerFlags.TcpNoDelay})
-        #.withPlainText()
-        .withNoise()
-        .build()
-    gossipSub = GossipSub.init(
-      switch = switch,
-#      triggerSelf = true,
-      msgIdProvider = msgIdProvider,
-      verifySignature = false,
-      anonymize = true,
-      )
-    pingProtocol = Ping.new(rng=rng)
-  # Metrics
+
+  # --- Node Setup ---
+  let myPort = 5000 + myId
+  let myAddress = "0.0.0.0:" & $myPort
+  let address = initTAddress(myAddress)
+  let switch =
+    SwitchBuilder.new()
+      .withAddress(MultiAddress.init(address).tryGet())
+      .withRng(rng)
+      .withMplex()
+      .withMaxConnections(250)
+      .withTcpTransport(flags = {ServerFlags.TcpNoDelay})
+      .withNoise()
+      .build()
+
+  let gossipSub = GossipSub.init(
+    switch = switch,
+    msgIdProvider = msgIdProvider,
+    verifySignature = false,
+    anonymize = true
+  )
+  let pingProtocol = Ping.new(rng = rng)
+
+  # --- Metrics ---
   echo "Starting metrics HTTP server"
   let metricsServer = startMetricsServer(parseIpAddress("0.0.0.0"), Port(8008))
 
+  # --- GossipSub Parameters ---
   gossipSub.parameters.floodPublish = true
-  #gossipSub.parameters.lazyPushThreshold = 1_000_000_000
-  #gossipSub.parameters.lazyPushThreshold = 0
   gossipSub.parameters.opportunisticGraftThreshold = -10000
   gossipSub.parameters.heartbeatInterval = 1.seconds
   gossipSub.parameters.pruneBackoff = 60.seconds
@@ -84,7 +77,7 @@ proc main {.async.} =
   gossipSub.parameters.dLow = 4
   gossipSub.parameters.dHigh = 8
   gossipSub.parameters.dScore = 6
-  gossipSub.parameters.dOut = 6 div 2
+  gossipSub.parameters.dOut = 3
   gossipSub.parameters.dLazy = 6
   gossipSub.topicParams["test"] = TopicParams(
     topicWeight: 1,
@@ -93,63 +86,38 @@ proc main {.async.} =
     firstMessageDeliveriesDecay: 0.9
   )
 
+  # --- Message Handling ---
   var messagesChunks: CountTable[uint64]
   proc messageHandler(topic: string, data: seq[byte]) {.async.} =
     let sentUint = uint64.fromBytesLE(data)
-    # warm-up
-    if sentUint < 1000000: return
-    #if isAttacker: return
-
+    if sentUint < 1000000: return # warm-up
     messagesChunks.inc(sentUint)
     if messagesChunks[sentUint] < chunks: return
-    let
-      sentMoment = nanoseconds(int64(uint64.fromBytesLE(data)))
-      sentNanosecs = nanoseconds(sentMoment - seconds(sentMoment.seconds))
-      sentDate = initTime(sentMoment.seconds, sentNanosecs)
-      diff = getTime() - sentDate
+    let sentMoment = nanoseconds(int64(uint64.fromBytesLE(data)))
+    let sentNanosecs = nanoseconds(sentMoment - seconds(sentMoment.seconds))
+    let sentDate = initTime(sentMoment.seconds, sentNanosecs)
+    let diff = getTime() - sentDate
     echo sentUint, " milliseconds: ", diff.inMilliseconds()
 
-
-  var
-    startOfTest: Moment
-    attackAfter = 10000.hours
   proc messageValidator(topic: string, msg: Message): Future[ValidationResult] {.async.} =
-    if isAttacker and Moment.now - startOfTest >= attackAfter:
-      return ValidationResult.Ignore
-
-    return ValidationResult.Accept
+    ValidationResult.Accept
 
   gossipSub.subscribe("test", messageHandler)
   gossipSub.addValidator(["test"], messageValidator)
   switch.mount(gossipSub)
   switch.mount(pingProtocol)
   await switch.start()
-  #defer: await switch.stop()
 
   echo "Listening on ", switch.peerInfo.addrs
   echo myId, ", ", isPublisher, ", ", switch.peerInfo.peerId
-  echo "Waiting 60 seconds for node building..."
-  await sleepAsync(60.seconds)
+  echo "Waiting 20 seconds for node building..."
+  await sleepAsync(20.seconds)
 
-  proc pinger(peerId: PeerId) {.async.} =
-    try:
-      await sleepAsync(20.seconds)
-      while true:
-        let stream = await switch.dial(peerId, PingCodec)
-        let delay = await pingProtocol.ping(stream)
-        await stream.close()
-        #echo delay
-        await sleepAsync(delay)
-    except:
-      echo "Failed to ping"
-
-
+  # --- Peer Discovery & Connection ---
   let connectTo = parseInt(getEnv("CONNECTTO"))
   var connected = 0
   var addrs: seq[MultiAddress]
-
-  # Discover other pods by their Docker hostnames
-  for i in 0 ..< parseInt(getEnv("PEERS")):
+  for i in 0 ..< publisherCount:
     if i == myId: continue # skip self
     let podAddr = "pod-" & $i & ":" & $(5000 + i)
     try:
@@ -158,16 +126,14 @@ proc main {.async.} =
       echo podAddr, " resolved: ", resolved
     except CatchableError as exc:
       echo "Failed to resolve address:", podAddr, " ", exc.msg
-
   rng.shuffle(addrs)
+
   var index = 0
-  while true:
-    if connected >= connectTo: break
+  while connected < connectTo:
     while true:
       try:
         echo "Trying to connect to ", addrs[index]
-        let peerId = await switch.connect(addrs[index], allowUnknownPeerId=true).wait(5.seconds)
-        #asyncSpawn pinger(peerId)
+        let peerId = await switch.connect(addrs[index], allowUnknownPeerId = true).wait(5.seconds)
         connected.inc()
         index.inc()
         echo "Connected!"
@@ -177,26 +143,18 @@ proc main {.async.} =
         echo "Waiting 15 seconds..."
         await sleepAsync(15.seconds)
 
-  #let
-  #  maxMessageDelay = client.param(int, "max_message_delay")
-  #  warmupMessages = client.param(int, "warmup_messages")
-  #startOfTest = Moment.now() + milliseconds(warmupMessages * maxMessageDelay div 2)
-
   echo "Mesh size: ", gossipSub.mesh.getOrDefault("test").len
 
+  # --- Message Publishing ---
   let turnToPublish = parseInt(getHostname()[4..^1])
   echo "Publishing turn is: ", turnToPublish
-  for msg in 0 ..< 10000:#client.param(int, "message_count"):
-    await sleepAsync(msg_rate)
+  for msg in 0 ..< 10:
+    await sleepAsync(msgRate)
     if msg mod publisherCount == turnToPublish:
-      echo "Sending message at: " ,times.getTime()
-      let
-        now = getTime()
-        nowInt = seconds(now.toUnix()) + nanoseconds(times.nanosecond(now))
-      var nowBytes = @(toBytesLE(uint64(nowInt.nanoseconds))) & newSeq[byte](msg_size)
+      echo "Sending message at: ", times.getTime()
+      let now = getTime()
+      let nowInt = seconds(now.toUnix()) + nanoseconds(nanosecond(now))
+      var nowBytes = @(toBytesLE(uint64(nowInt.nanoseconds))) & newSeq[byte](msgSize)
       doAssert((await gossipSub.publish("test", nowBytes)) > 0)
-
-  #echo "BW: ", libp2p_protocols_bytes.value(labelValues=["/meshsub/1.1.0", "in"]) + libp2p_protocols_bytes.value(labelValues=["/meshsub/1.1.0", "out"])
-  #echo "DUPS: ", libp2p_gossipsub_duplicate.value(), " / ", libp2p_gossipsub_received.value()
 
 waitFor(main())
