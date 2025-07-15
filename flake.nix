77ef345f26e9df1f6e275c573eb0f728cdda90c7
Ben-PH <benphawke@gmail.com>
2025-07-15 19:47:32 +0200

    chore: Remove some TODOs to GH issue #1545

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          openssl.dev
+        ];
+      };
+    };
+}
--- libp2p/protocols/kademlia/kademlia.nim
+++ libp2p/protocols/kademlia/kademlia.nim
@@ -66,7 +66,6 @@ proc findNode*(
     kad: KadDHT, targetId: Key
 ): Future[seq[PeerId]] {.async: (raises: [CancelledError]).} =
   #debug "findNode", target = target
-  # TODO: check if already exist in rtable
   # TODO: should it return a single peer instead? read spec
 
   var initialPeers = kad.rtable.findClosestPeers(targetId, k)
@@ -84,7 +83,6 @@ proc findNode*(
       state.markPending(peer)
 
       pendingFutures[peer] = kad.sendFindNode(peer, targetId).wait(5.seconds)
-      # TODO: should this timeout be specified by the dev? maybe should be a config option
 
       state.activeQueries.inc
 
@@ -110,7 +108,6 @@ proc findNode*(
 proc bootstrap*(
     kad: KadDHT, bootstrapNodes: seq[PeerInfo]
 ) {.async: (raises: [CancelledError]).} =
-  # TODO: every 10 minutes configurable, run once on start
   for b in bootstrapNodes:
     try:
       await kad.switch.connect(b.peerId, b.addrs)
@@ -175,7 +172,6 @@ proc new*(
           discard kad.rtable.insert(conn.peerId)
         else:
           raise newException(LPError, "unhandled kad-dht message type")
-        # TODO: implement other types
     except CancelledError as exc:
       raise exc
     except CatchableError:
