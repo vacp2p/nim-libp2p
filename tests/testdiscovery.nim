c788b6a34f5ceb5842821f9b05ca3b096f656ab3
Ben-PH <benphawke@gmail.com>
2025-07-23 21:12:43 +0200

    run failing tests

--- tests/testdiscovery.nim
+++ tests/testdiscovery.nim
@@ -13,13 +13,24 @@ import options, chronos, sets
 import
   ../libp2p/[
     protocols/rendezvous,
+    protocols/kademlia,
     switch,
     builders,
     discovery/discoverymngr,
     discovery/rendezvousinterface,
+    discovery/discoverykad,
   ]
 import ./helpers, ./utils/async_tests
 
+proc createDSwitch(): Switch =
+  SwitchBuilder
+  .new()
+  .withRng(newRng())
+  .withAddresses(@[MultiAddress.init("/ip4/0.0.0.0/tcp/0").tryGet()])
+  .withTcpTransport()
+  .withMplex()
+  .withNoise()
+  .build()
 proc createSwitch(rdv: RendezVous = RendezVous.new()): Switch =
   SwitchBuilder
   .new()
@@ -54,6 +65,37 @@ suite "Discovery":
 
     let
       query = dmA.request(RdvNamespace("foo"))
+      res = await query.getPeer()
+    check:
+      res{PeerId}.get() == clientB.peerInfo.peerId
+      res[PeerId] == clientB.peerInfo.peerId
+      res.getAll(PeerId) == @[clientB.peerInfo.peerId]
+      toHashSet(res.getAll(MultiAddress)) == toHashSet(clientB.peerInfo.addrs)
+    await allFutures(clientA.stop(), clientB.stop(), remoteNode.stop())
+
+  asyncTest "Kad test":
+    let clientA = createDSwitch()
+    let clientB = createDSwitch()
+    let kadA = KadDHT.new(clientA)
+    let kadB = KadDHT.new(clientB)
+    let remoteNode = createDSwitch()
+    let dmA = DiscoveryManager()
+    let dmB = DiscoveryManager()
+    clientA.mount(kadA)
+    clientB.mount(kadB)
+    let kda: KadDiscovery = KadDiscovery.new(kadA)
+    dmA.add(kda)
+    dmB.add(KadDiscovery.new(kadB))
+    await allFutures(clientA.start(), clientB.start(), remoteNode.start())
+
+    await clientB.connect(remoteNode.peerInfo.peerId, remoteNode.peerInfo.addrs)
+    await clientA.connect(remoteNode.peerInfo.peerId, remoteNode.peerInfo.addrs)
+
+    # dmB.advertise(KadNamespace("hardcoded"))
+
+    let
+      # TODO: this calls the unimplemented base, but I want to call the inherited method...
+      query = dmA.request(KadNamespace("hardcoded"))
       res = await query.getPeer()
     check:
       res{PeerId}.get() == clientB.peerInfo.peerId
