37168d8027f4f299b0aa9658bd7500d9a56fed31
Ben-PH <benphawke@gmail.com>
2025-07-25 12:15:36 +0200

    fix types

--- libp2p/protocols/kademlia/dhttypes.nim
+++ libp2p/protocols/kademlia/dhttypes.nim
@@ -1,25 +1,27 @@
 import std/[tables, hashes]
 
-type EntryKey = object
-  data: seq[byte]
+type EntryKey* = object
+  data*: seq[byte]
 
-type EntryVal = object
-  data: seq[byte]
+type EntryVal* = object
+  data*: seq[byte]
 
-type TimeStamp = object
+type TimeStamp* = object
   # Currently a string, because for some reason, that's what is chosen at the protobuf level
   # TODO: convert between RFC3339 strings and use of integers (i.e. the _correct_ way)
-  ts: string
+  ts*: string
 
-type EntryCandidate = object
+type EntryCandidate* = object
   key*: EntryKey
   val*: EntryVal
   time*: TimeStamp
 
+type BaseValidator* = object
+
 type ValidatedEntry = object
-  key: EntryKey
-  val: EntryVal
-  time: TimeStamp
+  key*: EntryKey
+  val*: EntryVal
+  time*: TimeStamp
 
 type RecordVal = object
   value: EntryVal
@@ -29,14 +31,12 @@ type LocalTable* = object
   entries: Table[EntryKey, (EntryVal, TimeStamp)]
 
 proc init*(self: typedesc[LocalTable]): LocalTable {.raises: [].} =
-  LocalTable(entries: Table.init())
+  LocalTable(entries: initTable[EntryKey, (EntryVal, TimeStamp)]())
 
 proc insert*(self: var LocalTable, val: sink ValidatedEntry) =
   self.entries[val.key] = (val.val, val.time)
 
-type BaseValidator = object
-
 proc validate*(
-    self: typedesc[BaseValidator], candidate: sink EntryCandidate
+    self: BaseValidator, candidate: sink EntryCandidate
 ): ValidatedEntry {.raises: [].} =
   doAssert(false, "[BaseValidator.advertise] abstract method not implemented!")
