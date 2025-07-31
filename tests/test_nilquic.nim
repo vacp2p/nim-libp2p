c435145302680eea6365f444bc1ae3d88a325a9c
vlado <vlado@brrrrr.at>
2025-07-31 18:52:27 +0200

    save

--- tests/test_nilquic.nim
+++ tests/test_nilquic.nim
@@ -1,25 +1,12 @@
 {.used.}
 
-import std/[sequtils]
+import utils/async_tests
 import pubsub/utils
-import ../libp2p/protocols/pubsub/[gossipsub]
-import helpers
 
 suite "quic":
-  teardown:
-    checkTrackers()
-
   asyncTest "Nil":
-    const
-      numberOfNodes = 10
-      topic = "foobar"
-      heartbeatInterval = 200.milliseconds
-    let
-      nodes = generateNodes(
-          numberOfNodes, gossip = true, heartbeatInterval = heartbeatInterval
-        )
-        .toGossipSub()
-      node0 = nodes[0]
+    const numberOfNodes = 10
+    let nodes = generateNodes(numberOfNodes, gossip = true).toGossipSub()
 
     startNodesAndDeferStop(nodes)
     await connectNodesStar(nodes)
