4de09ac0f48ae3e14a60049457dcd888b7c10024
Ben-PH <benphawke@gmail.com>
2025-08-04 18:52:25 +0200

    chore: Revert silencing of other tests

--- tests/testnative.nim
+++ tests/testnative.nim
@@ -9,32 +9,32 @@
 # This file may not be copied, modified, or distributed except according to
 # those terms.
 
-# import
-#   testvarint, testconnection, testbridgestream, testminprotobuf, testsemaphore,
-#   testheartbeat, testfuture, testzeroqueue, testbytesview
-#
-# import testminasn1, testrsa, testecnist, tested25519, testsecp256k1, testcrypto
-#
-# import
-#   testmultibase, testmultihash, testmultiaddress, testcid, testpeerid,
-#   testsigned_envelope, testrouting_record
-#
-# import
-#   testtcptransport,
-#   testtortransport,
-#   testwstransport,
-#   testquic,
-#   testmemorytransport,
-#   transports/tls/testcertificate
-#
-# import
-#   testnameresolve, testmultistream, testbufferstream, testidentify,
-#   testobservedaddrmanager, testconnmngr, testswitch, testnoise, testpeerinfo,
-#   testpeerstore, testping, testmplex, testrelayv1, testrelayv2, testrendezvous,
-#   testdiscovery, testyamux, testautonat, testautonatservice, testautorelay, testdcutr,
-#   testhpservice, testutility, testhelpers, testwildcardresolverservice, testperf
-#
+import
+  testvarint, testconnection, testbridgestream, testminprotobuf, testsemaphore,
+  testheartbeat, testfuture, testzeroqueue, testbytesview
+
+import testminasn1, testrsa, testecnist, tested25519, testsecp256k1, testcrypto
+
+import
+  testmultibase, testmultihash, testmultiaddress, testcid, testpeerid,
+  testsigned_envelope, testrouting_record
+
+import
+  testtcptransport,
+  testtortransport,
+  testwstransport,
+  testquic,
+  testmemorytransport,
+  transports/tls/testcertificate
+
+import
+  testnameresolve, testmultistream, testbufferstream, testidentify,
+  testobservedaddrmanager, testconnmngr, testswitch, testnoise, testpeerinfo,
+  testpeerstore, testping, testmplex, testrelayv1, testrelayv2, testrendezvous,
+  testdiscovery, testyamux, testautonat, testautonatservice, testautorelay, testdcutr,
+  testhpservice, testutility, testhelpers, testwildcardresolverservice, testperf
+
 import kademlia/[testencoding, testroutingtable, testfindnode, testputval]
-#
-# when defined(libp2p_autotls_support):
-#   import testautotls
+
+when defined(libp2p_autotls_support):
+  import testautotls
