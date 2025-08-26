9865cc39b5f27de6e819178de353bf9a6b557894
vladopajic <vladopajic@users.noreply.github.com>
2025-08-26 16:00:25 +0200

    chore(perf): follow up for PR#1600 (#1620)

--- libp2p/protocols/perf/server.nim
+++ libp2p/protocols/perf/server.nim
@@ -24,42 +24,29 @@ type Perf* = ref object of LPProtocol
 
 proc new*(T: typedesc[Perf]): T {.public.} =
   var p = T()
+
   proc handle(conn: Connection, proto: string) {.async: (raises: [CancelledError]).} =
-    var bytesRead = 0
     try:
       trace "Received benchmark performance check", conn
-      var
-        sizeBuffer: array[8, byte]
-        size: uint64
-      await conn.readExactly(addr sizeBuffer[0], 8)
-      size = uint64.fromBytesBE(sizeBuffer)
 
       var uploadSizeBuffer: array[8, byte]
       await conn.readExactly(addr uploadSizeBuffer[0], 8)
+      var uploadSize = uint64.fromBytesBE(uploadSizeBuffer)
 
-      # Read exact amount with fallback to EOF for half-close compatibility
-      # Doesn't quite match perf, but without this the quic test is flaky.
-      # Sometimes a deadlock occurs
-      # TODO: Remove it
-      let uploadSize = uint64.fromBytesBE(uploadSizeBuffer)
-      var toReadBuffer: array[PerfSize, byte]
-      var remainingToRead = uploadSize
-
-      while remainingToRead > 0:
-        let toRead = min(remainingToRead, PerfSize.uint64)
-        let actualRead = await conn.readOnce(addr toReadBuffer[0], toRead.int)
-
-        if actualRead == 0:
+      var readBuffer: array[PerfSize, byte]
+      while not conn.atEof:
+        try:
+          let readBytes = await conn.readOnce(addr readBuffer[0], PerfSize)
+          if readBytes == 0:
+            break
+        except LPStreamEOFError:
           break
 
-        bytesRead += actualRead
-        remainingToRead -= actualRead.uint64
-
-      var buf: array[PerfSize, byte]
-      while size > 0:
-        let toWrite = min(size, PerfSize)
-        await conn.write(buf[0 ..< toWrite])
-        size -= toWrite
+      var writeBuffer: array[PerfSize, byte]
+      while uploadSize > 0:
+        let toWrite = min(uploadSize, PerfSize)
+        await conn.write(writeBuffer[0 ..< toWrite])
+        uploadSize -= toWrite
     except CancelledError as exc:
       trace "cancelled perf handler"
       raise exc
