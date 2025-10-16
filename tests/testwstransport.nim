79f18bf9bc5a0fcec8f79dca900193daf7629014
richΛrd <info@richardramos.me>
2025-10-15 23:19:41 -0400

    feat: use BoringSSL for certificate ops (#1761)

--- tests/testwstransport.nim
+++ tests/testwstransport.nim
@@ -140,13 +140,7 @@ suite "WebSocket transport":
 
 when defined(libp2p_autotls_support):
   import bearssl/pem
-  import
-    ../libp2p/[
-      autotls/service,
-      autotls/mockservice,
-      transports/tls/certificate,
-      transports/tls/certificate_ffi,
-    ]
+  import ../libp2p/[autotls/service, autotls/mockservice, transports/tls/certificate]
 
   proc generateCertAndKey(key: KeyPair): (TLSPrivateKey, TLSCertificate) =
     let certDer = generateX509(key, encodingFormat = DER).certificate
