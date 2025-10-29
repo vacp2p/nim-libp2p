1de4a80ca61a92d13e053d21e157482b7b16918d
Vlado Pajić <vlado@brrrrr.at>
2025-10-29 13:59:08 +0100

    avoid mysterious shadowing

--- tests/mix/testmixnode.nim
+++ tests/mix/testmixnode.nim
@@ -9,7 +9,7 @@ suite "Mix Node Tests":
 
   setup:
     const count = 5
-    let mixNodes = initializeMixNodes(count).expect("could not generate mix nodes")
+    mixNodes = initializeMixNodes(count).expect("could not generate mix nodes")
     check:
       mixNodes.len == count
 
@@ -18,7 +18,7 @@ suite "Mix Node Tests":
     deletePubInfoFolder()
 
   test "get mix node by index":
-    for i in 0 ..< count:
+    for i in 0 ..< mixNodes.len:
       let node = mixNodes[i]
       let
         (peerId, multiAddr, mixPubKey, mixPrivKey, libp2pPubKey, libp2pPrivKey) =
@@ -35,7 +35,7 @@ suite "Mix Node Tests":
         libp2pPrivKey.getBytes().len == SkRawPrivateKeySize
 
   test "find mixnode by peerid":
-    for i in 0 ..< count:
+    for i in 0 ..< mixNodes.len:
       let
         node = mixNodes[i]
         foundNode = mixNodes.findByPeerId(node.peerId).expect("find mix node error")
@@ -49,7 +49,7 @@ suite "Mix Node Tests":
       mixNodes.findByPeerId(peerId).isErr()
 
   test "write and read mixnodeinfo":
-    for i in 0 ..< count:
+    for i in 0 ..< mixNodes.len:
       let node = mixNodes[i]
       node.writeToFile(i).expect("File write error")
       let readNode = MixNodeInfo.readFromFile(i).expect("Read node error")
@@ -58,7 +58,7 @@ suite "Mix Node Tests":
         readNode == node
 
   test "write and read mixpubinfo":
-    for i in 0 ..< count:
+    for i in 0 ..< mixNodes.len:
       let mixPubInfo =
         mixNodes.getMixPubInfoByIndex(i).expect("couldnt obtain mixpubinfo")
       mixPubInfo.writeToFile(i).expect("File write error")
@@ -77,7 +77,7 @@ suite "Mix Node Tests":
     let mixNodes =
       initializeMixNodes(count, basePort).expect("could not generate mix nodes")
 
-    for i in 0 ..< count:
+    for i in 0 ..< mixNodes.len:
       let tcpPort = mixNodes[i].multiAddr.getPart(multiCodec("tcp")).value()
       let expectedPort = MultiAddress.init(fmt"/tcp/{basePort + i}").tryGet()
       check:
