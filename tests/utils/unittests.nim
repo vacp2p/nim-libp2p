87566aa618e4e9ff525da49332bfaa00c93ea62a
Radoslaw Kaminski <radoslaw@status.im>
2025-10-29 15:33:06 +0000

    test: add comment

--- tests/utils/unittests.nim
+++ tests/utils/unittests.nim
@@ -1,7 +1,19 @@
+## Use this module instead of unittest2 directly in tests.
+## It wraps unittest2.suite in a proc to avoid issue with too many global variables
+## See https://github.com/nim-lang/Nim/issues/8500
+
 import unittest2
 import chronos
 
 export unittest2 except suite
+
+template suite*(name: string, body: untyped): untyped =
+  block:
+    proc testSuite() =
+      unittest2.suite name:
+        body
+
+    testSuite()
 
 template asyncTeardown*(body: untyped): untyped =
   teardown:
@@ -29,11 +41,3 @@ template asyncTest*(name: string, body: untyped): untyped =
           body
       )()
     )
-
-template suite*(name: string, body: untyped): untyped =
-  block:
-    proc testSuite() =
-      unittest2.suite name:
-        body
-
-    testSuite()
