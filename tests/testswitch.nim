8e50814fa6809cd3e49108c52d89bde07eb6b5a7
vladopajic <vladopajic@users.noreply.github.com>
2025-10-29 14:31:42 +0100

    Update tests/testswitch.nim

--- tests/testswitch.nim
+++ tests/testswitch.nim
@@ -374,8 +374,7 @@ suite "Switch":
 
         check peerId == switch2.peerInfo.peerId
       else:
-        raiseAssert "Connection event hook called more than expected (step = " & $step &
-          ")"
+       raiseAssert "Connection event hook called more than expected"
 
       step.inc()
 
