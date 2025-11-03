7489a39a1205378ec3433752c50561676cc54eb9
Vlado Pajić <vlado@brrrrr.at>
2025-11-03 12:47:26 +0100

    fix headers

--- libp2p/protocols/mix/mix_protocol.nim
+++ libp2p/protocols/mix/mix_protocol.nim
@@ -1,3 +1,12 @@
+# Nim-LibP2P
+# Copyright (c) 2023-2025 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0 ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 import chronicles, chronos, sequtils, os, results, sets
 import std/[strformat, tables], metrics
 import
