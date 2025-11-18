44d3b45b65ef8fa61cc922b233b3fe37b6542b57
Gabriel Cruz <gabe@gmelodie.com>
2025-11-18 20:00:02 -0300

    chore(kad): interop tests

---
+++ interop/kad/rust-peer/src/main.rs
@@ -0,0 +1,57 @@
+// Nim-LibP2P
+// Copyright (c) 2023-2025 Status Research & Development GmbH
+// Licensed under either of
+//  * Apache License, version 2.0 ([LICENSE-APACHE](LICENSE-APACHE))
+//  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+// at your option.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
+
+use libp2p::{
+    futures::StreamExt,
+    identity,
+    kad::{self, store::MemoryStore},
+    noise,
+    swarm::SwarmEvent,
+    tcp, yamux, Multiaddr, PeerId,
+};
+use std::{error::Error, fs};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local PeerId: {local_peer_id}");
+
+    fs::write("peer.id", local_peer_id.to_string())?;
+
+    let mut swarm = libp2p::SwarmBuilder::with_existing_identity(local_key)
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|_| kad::Behaviour::new(local_peer_id, MemoryStore::new(local_peer_id)))?
+        .build();
+
+    let listen_addr: Multiaddr = "/ip4/0.0.0.0/tcp/4141".parse()?;
+    swarm.listen_on(listen_addr)?;
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::Behaviour(kad::Event::OutboundQueryProgressed { result, .. }) => {
+                if let kad::QueryResult::PutRecord(res) = result {
+                    if res?.key.as_ref() == b"rust-peer" {
+                        println!("Received PUT_VALUE for rust-peer!");
+                        fs::File::create("success")?;
+                    }
+                }
+            }
+            SwarmEvent::NewListenAddr { address, .. } => {
+                println!("Listening on {address}");
+            }
+            _ => {}
+        }
+    }
+}
