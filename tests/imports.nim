b275922e7049a12fbe4502012075cb50b506a3f0
Radoslaw Kaminski <radoslaw@status.im>
2025-11-26 15:59:21 +0000

    add imports macro

---
+++ tests/imports.nim
@@ -0,0 +1,24 @@
+import std/macros
+import std/os
+import std/strutils
+import std/sequtils
+
+macro importTests*(
+    dir: static string, ignorePaths: static openArray[string] = []
+): untyped =
+  ## imports all files in the specified directory whose filename
+  ## starts with "test_" and ends in ".nim"
+  ## ignores test_all files and any paths listed in ignorePaths
+  let imports = newStmtList()
+  for file in walkDirRec(dir):
+    let (path, name, ext) = splitFile(file)
+    let isTestFile = name.startsWith("test_") and not name.startsWith("test_all")
+    let isNimFile = ext == ".nim"
+    let isIgnored = ignorePaths.anyIt(path.contains(it))
+
+    if isTestFile and isNimFile and not isIgnored:
+      imports.add(
+        quote do:
+          import `file`
+      )
+  imports
