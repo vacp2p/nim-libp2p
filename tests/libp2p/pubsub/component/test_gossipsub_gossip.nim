f7756ba323f619042fb84e052c4bc9fd5a59bfa5
Radosław Kamiński <radoslaw@status.im>
2025-12-05 14:13:27 +0000

    Update tests/libp2p/pubsub/component/test_gossipsub_gossip.nim
    Co-authored-by: richΛrd <info@richardramos.me>

--- tests/libp2p/pubsub/component/test_gossipsub_gossip.nim
+++ tests/libp2p/pubsub/component/test_gossipsub_gossip.nim
@@ -11,7 +11,7 @@
 
 import chronos, std/[sequtils], stew/byteutils
 import ../../../../libp2p/protocols/pubsub/[gossipsub, mcache, peertable, rpc/message]
-import ../../../tools/[unittest]
+import ../../../tools/unittest
 import ../utils
 
 suite "GossipSub Component - Gossip Protocol":
