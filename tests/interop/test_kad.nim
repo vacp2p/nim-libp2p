0995ca8b7516b787f91be1b906fd3ac451588814
Eshaan <eshaan28gupta@gmail.com>
2025-12-28 02:21:33 +1100

    test: refactor kadInteropTest to shared reusable proc
    - Created tests/interop/kadTests.nim with reusable kadInteropTest proc
    - Refactored tests/interop/test_kad.nim to use shared proc and reuse node
    - Refactored interop/kad/nim-peer/src/nim_peer.nim to use shared proc
    - Reduced code duplication by 199 lines
    - Same testing logic now used for both nim-to-nim and nim-to-rust interop
    
    Addresses feedback from @vladopajic in #1990

--- tests/interop/test_kad.nim
+++ tests/interop/test_kad.nim
@@ -12,59 +12,18 @@
 import chronos, stew/byteutils
 import ../../libp2p/[switch, builders, peerid, protocols/kademlia, wire]
 import ../tools/[unittest]
-
-proc kadInteropTest(otherPeerId: PeerId, otherAddr: string, ourAddr: string): Future[bool] {.async.} =
-  ## Tests Kademlia DHT interoperability by:
-  ## 1. Creating a local switch with Kademlia
-  ## 2. Connecting to another peer
-  ## 3. Putting a value into the DHT
-  ## 4. Retrieving the value to verify it was stored correctly
-  var switch = SwitchBuilder
-    .new()
-    .withRng(newRng())
-    .withAddresses(@[MultiAddress.init(ourAddr).tryGet()])
-    .withTcpTransport()
-    .withMplex()
-    .withNoise()
-    .build()
-
-  let kad = KadDHT.new(
-    switch,
-    bootstrapNodes = @[(otherPeerId, @[MultiAddress.init(otherAddr).get()])],
-    config = KadDHTConfig.new(quorum = 1),
-  )
-
-  switch.mount(kad)
-
-  await switch.start()
-  defer:
-    await switch.stop()
-
-  let key: Key = "key".toBytes()
-  let value = "value".toBytes()
-
-  let res = await kad.putValue(key, value)
-  if res.isErr():
-    echo "putValue failed: ", res.error
-    return false
-
-  # wait for other peer's kad to store the value
-  await sleepAsync(2.seconds)
-
-  # try to get the inserted value from peer
-  if (await kad.getValue(key)).get().value != value:
-    echo "Get value did not return correct value"
-    return false
-
-  return true
+import ./kadTests
 
 suite "KadDHT Interop Tests":
-  teardown:
-    checkTrackers()
+  # Create and keep a single node running for all tests
+  var switch1: Switch
+  var kad1: KadDHT
+  var peer1Id: PeerId
+  var peer1Addr: string
 
-  asyncTest "Success path - two nim nodes":
-    # Create first node
-    let switch1 = SwitchBuilder
+  setup:
+    # Create first node once and reuse it across tests
+    switch1 = SwitchBuilder
       .new()
       .withRng(newRng())
       .withAddresses(@[MultiAddress.init("/ip4/127.0.0.1/tcp/0").tryGet()])
@@ -73,117 +32,24 @@ suite "KadDHT Interop Tests":
       .withNoise()
       .build()
 
-    let kad1 = KadDHT.new(
-      switch1,
-      bootstrapNodes = @[],
-      config = KadDHTConfig.new(quorum = 1),
+    kad1 = KadDHT.new(
+      switch1, bootstrapNodes = @[], config = KadDHTConfig.new(quorum = 1)
     )
 
     switch1.mount(kad1)
     await switch1.start()
-
-    defer:
-      await switch1.stop()
 
     # Get the actual address and peer ID of the first node
-    let peer1Id = switch1.peerInfo.peerId
-    let peer1Addr = switch1.peerInfo.addrs[0]
+    peer1Id = switch1.peerInfo.peerId
+    peer1Addr = $switch1.peerInfo.addrs[0]
 
-    # Test kadInteropTest with the first node as the "other" peer
+  teardown:
+    await switch1.stop()
+    checkTrackers()
+
+  asyncTest "kadInteropTest with nim node":
     let ourAddr = "/ip4/127.0.0.1/tcp/0"
-    let result = await kadInteropTest(peer1Id, $peer1Addr, ourAddr)
 
+    # Test using the kadInteropTest proc (same one used for rust interop)
+    let result = await kadInteropTest(peer1Id, peer1Addr, ourAddr)
     check result == true
-
-  asyncTest "Failure path - invalid peer ID":
-    # Create a random peer ID that doesn't correspond to any running node
-    let invalidPeerId = PeerId.random().expect("Valid PeerId")
-    let nonExistentAddr = "/ip4/127.0.0.1/tcp/9999"
-    let ourAddr = "/ip4/127.0.0.1/tcp/0"
-
-    # This should fail because the peer doesn't exist
-    # We expect this to return false or timeout
-    let switch = SwitchBuilder
-      .new()
-      .withRng(newRng())
-      .withAddresses(@[MultiAddress.init(ourAddr).tryGet()])
-      .withTcpTransport()
-      .withMplex()
-      .withNoise()
-      .build()
-
-    let kad = KadDHT.new(
-      switch,
-      bootstrapNodes = @[(invalidPeerId, @[MultiAddress.init(nonExistentAddr).get()])],
-      config = KadDHTConfig.new(quorum = 1),
-    )
-
-    switch.mount(kad)
-    await switch.start()
-    defer:
-      await switch.stop()
-
-    let key: Key = "test_key".toBytes()
-    let value = "test_value".toBytes()
-
-    # This should fail because there's no valid peer to connect to
-    let res = await kad.putValue(key, value)
-
-    # We expect this to fail since the bootstrap node doesn't exist
-    check res.isErr()
-
-  asyncTest "Failure path - unreachable address":
-    # Create a node with valid configuration
-    let switch1 = SwitchBuilder
-      .new()
-      .withRng(newRng())
-      .withAddresses(@[MultiAddress.init("/ip4/127.0.0.1/tcp/0").tryGet()])
-      .withTcpTransport()
-      .withMplex()
-      .withNoise()
-      .build()
-
-    let kad1 = KadDHT.new(
-      switch1,
-      bootstrapNodes = @[],
-      config = KadDHTConfig.new(quorum = 1),
-    )
-
-    switch1.mount(kad1)
-    await switch1.start()
-    defer:
-      await switch1.stop()
-
-    # Use the real peer ID but an unreachable address
-    let peer1Id = switch1.peerInfo.peerId
-    let unreachableAddr = "/ip4/127.0.0.1/tcp/9998" # Different port, won't work
-
-    # Create second node trying to connect to wrong address
-    let switch2 = SwitchBuilder
-      .new()
-      .withRng(newRng())
-      .withAddresses(@[MultiAddress.init("/ip4/127.0.0.1/tcp/0").tryGet()])
-      .withTcpTransport()
-      .withMplex()
-      .withNoise()
-      .build()
-
-    let kad2 = KadDHT.new(
-      switch2,
-      bootstrapNodes = @[(peer1Id, @[MultiAddress.init(unreachableAddr).get()])],
-      config = KadDHTConfig.new(quorum = 1),
-    )
-
-    switch2.mount(kad2)
-    await switch2.start()
-    defer:
-      await switch2.stop()
-
-    let key: Key = "test_key".toBytes()
-    let value = "test_value".toBytes()
-
-    # This should fail due to unreachable address
-    let res = await kad2.putValue(key, value)
-
-    # We expect this to fail
-    check res.isErr()
