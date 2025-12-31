d523b6096a4797830ef547ed02c2667fce8d770f
Radosław Kamiński <radoslaw@status.im>
2025-12-31 10:49:20 +0000

    test(interop): Autonatv2 test template (#1993)
    Co-authored-by: Vlado Pajić <vlado@brrrrr.at>
    Co-authored-by: vladopajic <vladopajic@users.noreply.github.com>

---
+++ tests/interop/test_autonatv2.nim
@@ -0,0 +1,72 @@
+# Nim-LibP2P
+# Copyright (c) 2023-2025 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0 ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+{.used.}
+
+import chronos
+import ./autonatv2
+import
+  ../../libp2p/
+    [builders, peerid, switch, wire, protocols/connectivity/autonatv2/service]
+import ../tools/[unittest]
+
+proc createSwitch(address: string, withAutonatV2: bool = true): Switch =
+  var builder = SwitchBuilder
+    .new()
+    .withRng(newRng())
+    .withAddresses(@[MultiAddress.init(address).get()])
+    .withTcpTransport()
+    .withYamux()
+    .withNoise()
+
+  if withAutonatV2:
+    builder = builder.withAutonatV2Server().withAutonatV2(
+        serviceConfig =
+          AutonatV2ServiceConfig.new(scheduleInterval = Opt.some(1.seconds))
+      )
+
+  builder.build()
+
+suite "Autonatv2 Interop Tests with Nim nodes":
+  asyncTeardown:
+    checkTrackers()
+
+  asyncTest "Happy path":
+    const peerAddress = "/ip6/::1/tcp/3030"
+    let switch = createSwitch(peerAddress, withAutonatV2 = true)
+
+    await switch.start()
+    defer:
+      await switch.stop()
+
+    const ourAddress = "/ip6/::1/tcp/4040"
+    check:
+      await autonatInteropTest(ourAddress, peerAddress, switch.peerInfo.peerId)
+
+  asyncTest "Timeout when peer lacks AutoNATv2 server":
+    const peerAddress = "/ip6/::1/tcp/3031"
+    let switch = createSwitch(peerAddress, withAutonatV2 = false)
+
+    await switch.start()
+    defer:
+      await switch.stop()
+
+    const ourAddress = "/ip6/::1/tcp/4041"
+    expect AsyncTimeoutError:
+      discard await autonatInteropTest(
+        ourAddress, peerAddress, switch.peerInfo.peerId, timeout = 3.seconds
+      )
+
+  asyncTest "Fails gracefully on connection error":
+    const unreachableAddress = "/ip6/::1/tcp/59999" # Nothing listening
+    let fakePeerId = PeerId.random().get()
+
+    const ourAddress = "/ip6/::1/tcp/4042"
+    expect DialFailedError:
+      discard await autonatInteropTest(ourAddress, unreachableAddress, fakePeerId)
