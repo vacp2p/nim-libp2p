27d67aaa63a39b5af27a6beeb0e1c4197d91cd43
Gabriel Cruz <gabe@gmelodie.com>
2026-01-21 15:06:03 -0300

    chore: add license

--- tools/gen-deps.sh
+++ tools/gen-deps.sh
@@ -1,4 +1,13 @@
+# Nim-LibP2P
+# Copyright (c) 2023-2026 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0 ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 usage() {
