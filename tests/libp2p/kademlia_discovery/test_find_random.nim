5c2651ebef5334b90f5babb844ba7a82148b7fa7
Radoslaw Kaminski <radoslaw@status.im>
2026-02-05 11:22:35 +0000

    migrate kad disco

--- tests/libp2p/kademlia_discovery/test_find_random.nim
+++ tests/libp2p/kademlia_discovery/test_find_random.nim
@@ -3,13 +3,10 @@
 
 {.used.}
 
-import chronicles, chronos, std/[sequtils, enumerate]
-import ../../../libp2p/[switch, builders]
+import chronos, std/sequtils
 import ../../../libp2p/protocols/[kademlia, kad_disco]
-import ../../tools/[unittest]
-import ./utils.nim
-
-trace "chronicles has to be imported to fix Error: undeclared identifier: 'activeChroniclesStream'"
+import ../../tools/[lifecycle, topology, unittest]
+import ./utils
 
 proc hasKey(kad: KademliaDiscovery, key: Key): bool =
   for b in kad.rtable.buckets:
@@ -23,38 +20,15 @@ suite "Kademlia discovery - FindRandom":
     checkTrackers()
 
   asyncTest "Simple find random node":
-    let swarmSize = 5
-    var switches: seq[Switch]
-    var kads: seq[KademliaDiscovery]
-    for i in 0 ..< swarmSize:
-      var (switch, kad) =
-        if i == 0:
-          setupKadSwitch(ExtEntryValidator(), ExtEntrySelector())
-        else:
-          setupKadSwitch(
-            ExtEntryValidator(),
-            ExtEntrySelector(),
-            @[(switches[0].peerInfo.peerId, switches[0].peerInfo.addrs)],
-          )
-      switches.add(switch)
-      kads.add(kad)
+    let kads = setupKads(5, ExtEntryValidator(),ExtEntrySelector())
+    startNodesAndDeferStop(kads)
 
-    # Bootstrapping needs to be done sequentially
-    for i in 1 ..< swarmSize:
-      await kads[i].bootstrap()
-
-    var entries = @[kads[0].rtable.selfId]
-
-    #  All the nodes that bootstropped off kad[0] have exactly 1 of each previous nodes, + kads[0], in their buckets
-    for i, kad in enumerate(kads[1 ..^ 1]):
-      for id in entries:
-        check kad.hasKey(id)
-      entries.add(kad.rtable.selfId)
+    connectNodesStar(kads, connectNodes)
 
     let records = await kads[1].randomRecords()
 
     check records.len == 4
+    let peerIds = kads.mapIt(it.switch.peerInfo.peerId)
     for record in records:
-      check switches.anyIt(it.peerInfo.peerId == record.peerId)
-
-    await switches.mapIt(it.stop()).allFutures()
+      check record.peerId in peerIds
+ 
