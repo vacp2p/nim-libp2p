e4e2bbc3f424255e9ef721f6dbe21f68837f6b76
SionoiS <simvivier@status.im>
2026-02-05 08:08:33 -0500

    add k closeast to randomRecords

--- libp2p/protocols/kademlia_discovery/randomfind.nim
+++ libp2p/protocols/kademlia_discovery/randomfind.nim
@@ -5,7 +5,7 @@ import std/[sequtils, sets]
 import chronos, chronicles, results
 import ../../[peerid, peerinfo, switch, multihash, routing_record, extended_peer_record]
 import ../protocol
-import ../kademlia/[types, find, get, protobuf]
+import ../kademlia/[types, find, get, protobuf, routingtable]
 import ./[types]
 
 logScope:
@@ -23,6 +23,15 @@ proc randomRecords*(
   let randomKey = randomPeerId.toKey()
 
   let queue = newAsyncQueue[(PeerId, Opt[Message])]()
+
+  let peers = disco.rtable.findClosestPeerIds(randomKey, disco.config.replication)
+  for peer in peers:
+    # queue cannot be full, we just created it...
+    # we add to the beginning of the queue to reverse the order
+    # since closer peers are lasts
+    let _ = catch:
+      queue.addFirstNoWait((peer, Opt.none(Message)))
+
   let findRes = catch:
     await disco.findNode(randomKey, queue)
   if findRes.isErr:
@@ -49,15 +58,17 @@ proc randomRecords*(
 
     buffers.add(buffer)
 
-  var records: seq[ExtendedPeerRecord]
+  # Since we start with K closest and also ask for closer peers
+  # we end up with duplicates.
+  var records: HashSet[ExtendedPeerRecord]
   for buffer in buffers:
     let sxpr = SignedExtendedPeerRecord.decode(buffer).valueOr:
       debug "cannot decode signed extended peer record", error
       continue
 
-    records.add(sxpr.data)
+    records.incl(sxpr.data)
 
-  return records
+  return records.toSeq()
 
 proc filterByServices*(
     records: seq[ExtendedPeerRecord], services: HashSet[ServiceInfo]
