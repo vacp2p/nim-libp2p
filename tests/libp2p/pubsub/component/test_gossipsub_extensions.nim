0f0bbbdc9c6bcfea709d6be2cd7b780d9cd94f06
vlado <vlado@brrrrr.at>
2026-02-06 16:01:19 -0300

    add test

--- tests/libp2p/pubsub/component/test_gossipsub_extensions.nim
+++ tests/libp2p/pubsub/component/test_gossipsub_extensions.nim
@@ -3,11 +3,11 @@
 
 {.used.}
 
-import chronos, algorithm
+import chronos, algorithm, stew/byteutils, sequtils
 import
-  ../../../../libp2p/protocols/pubsub/
-    [gossipsub, gossipsub/extensions, gossipsub/partial_message, rpc/message]
+  ../../../../libp2p/protocols/pubsub/[gossipsub, gossipsub/extensions, rpc/message]
 import ../../../tools/[lifecycle, unittest]
+import ../extensions/my_partial_message
 import ../utils
 
 suite "GossipSub Component - Extensions":
@@ -40,20 +40,25 @@ suite "GossipSub Component - Extensions":
         negotiatedPeersSorted == nodesPeerIdSorted
 
   asyncTest "Partial Message Extension":
-    proc unionPartsMetadata(
-        a, b: PartsMetadata
-    ): Result[PartsMetadata, string] {.gcsafe, raises: [].} =
-      err("unimplemented")
+    const topic = "logos-partial"
+    const groupId = "group-id-1".toBytes
 
     proc validateRPC(
         rpc: PartialMessageExtensionRPC
     ): Result[void, string] {.gcsafe, raises: [].} =
-      ok()
+      checkLen(rpc.partsMetadata)
+      return ok()
 
+    var incomingRPC: Table[PeerId, seq[PartialMessageExtensionRPC]]
     proc onIncomingRPC(
         peer: PeerId, rpc: PartialMessageExtensionRPC
     ) {.gcsafe, raises: [].} =
-      discard
+      # peer - who sent RPC (received from)
+      incomingRPC.mgetOrPut(peer, newSeq[PartialMessageExtensionRPC]()).add(rpc)
+
+      # note: ideally this is were applications will publish partial messages on requests.
+      # but for the sake of tests it is much more easier and intuitive to follow when 
+      # rpc are added to table, and testing is done in procedural, like in test below.
 
     let
       numberOfNodes = 2
@@ -62,10 +67,10 @@ suite "GossipSub Component - Extensions":
           gossip = true,
           partialMessageExtensionConfig = some(
             PartialMessageExtensionConfig(
-              unionPartsMetadata: unionPartsMetadata,
+              unionPartsMetadata: my_partial_message.unionPartsMetadata,
               validateRPC: validateRPC,
               onIncomingRPC: onIncomingRPC,
-              heartbeatsTillEviction: 3,
+              heartbeatsTillEviction: 100,
             )
           ),
         )
@@ -75,4 +80,49 @@ suite "GossipSub Component - Extensions":
 
     await connect(nodes[0], nodes[1])
 
-    # TODO
+    # subscribe all nodes requesting partial messages and wait for subscribe
+    for node in nodes:
+      node.subscribe(topic, voidTopicHandler, requestsPartial = true)
+    checkUntilTimeout:
+      nodes.allIt(it.gossipsub.getOrDefault(topic).len == 1)
+
+    # node 1 seeks for parts 1, 2, 3
+    let mpWant = MyPartialMessage(groupID: groupId, want: @[1, 2, 3])
+    await nodes[1].publishPartial(topic, mpWant)
+
+    # wait for node 0 to receive request
+    checkUntilTimeout:
+      # note: nodes[1] is used here because node 0 receives RPC from node 1.
+      # in other words, to get messages received by node 0, we need to 
+      # get messages that are sent by node 1.
+      incomingRPC.getOrDefault(nodes[1].peerInfo.peerId, @[]).len == 1
+
+    # assert that node 0 received exactly what node 1 sent
+    check:
+      incomingRPC[nodes[1].peerInfo.peerId][0] ==
+        PartialMessageExtensionRPC(
+          topicID: topic,
+          groupID: groupId,
+          partsMetadata: MyPartsMetadata.want(@[1, 2, 3]),
+        )
+
+    # then node 0 publishes data
+    let pmData = MyPartialMessage(
+      groupID: groupId,
+      data: {1: "one".toBytes, 2: "two".toBytes, 3: "three".toBytes}.toTable,
+    )
+    await nodes[0].publishPartial(topic, pmData)
+
+    # wait for node 1 to receive partial message
+    checkUntilTimeout:
+      incomingRPC.getOrDefault(nodes[0].peerInfo.peerId, @[]).len == 1
+
+    # assert that node 1 received exactly what node 0 sent
+    check:
+      incomingRPC[nodes[0].peerInfo.peerId][0] ==
+        PartialMessageExtensionRPC(
+          topicID: topic,
+          groupID: groupId,
+          partialMessage: "onetwothree".toBytes,
+          partsMetadata: MyPartsMetadata.have(@[1, 2, 3]),
+        )
