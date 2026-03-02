4ed237884d1548a32497fbff87d38ff66ddd6bbc
Arunima Chaudhuri <tinniarunima66@gmail.com>
2026-03-02 15:51:45 +0530

    fix

--- tests/libp2p/capability_discovery/test_discoverer.nim
+++ tests/libp2p/capability_discovery/test_discoverer.nim
@@ -4,6 +4,7 @@
 
 import std/options
 import chronos, results
+import std/sequtils
 import ../../../libp2p/protocols/kademlia_discovery/[types]
 import ../../../libp2p/protocols/capability_discovery/[discoverer, serviceroutingtables]
 import ../../tools/unittest
