b5dd6a8e1e189bbce9578688c249f378378ed287
Gabriel Cruz <gabe@gmelodie.com>
2026-04-02 09:52:34 -0300

    chore: remove sleep

--- tests/libp2p/protocols/test_autonat_v2_service.nim
+++ tests/libp2p/protocols/test_autonat_v2_service.nim
@@ -59,16 +59,10 @@ proc stopAll(switches: seq[Switch]) {.async.} =
 proc startAndConnect(switch: Switch, switches: seq[Switch]) {.async.} =
   await switch.start()
 
-  # Start all peers in parallel
   await switches.startAll()
 
-  await sleepAsync(200.millis)
-
-  # Connect sequentially (safer)
   for peer in switches:
     await switch.connect(peer.peerInfo.peerId, peer.peerInfo.addrs)
-
-  await sleepAsync(300.millis)
 
 proc newService(
     reachability: NetworkReachability,
