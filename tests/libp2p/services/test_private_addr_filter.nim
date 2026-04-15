dc10a1a19163c12556b749cab707fcf6e736a6ea
Richard Ramos <info@richardramos.me>
2026-04-15 18:13:02 -0400

    fix: test

--- tests/libp2p/services/test_private_addr_filter.nim
+++ tests/libp2p/services/test_private_addr_filter.nim
@@ -17,20 +17,11 @@ import
     peerstore,
     protocols/identify,
     protocols/kademlia,
-    protocols/rendezvous,
   ]
 import ../../tools/[unittest, crypto]
 
-# ---------------------------------------------------------------------------
-# Helper
-# ---------------------------------------------------------------------------
-
 proc ma(s: string): MultiAddress =
   MultiAddress.init(s).tryGet()
-
-# ---------------------------------------------------------------------------
-# Unit tests: isFilterablePrivateMA
-# ---------------------------------------------------------------------------
 
 suite "isFilterablePrivateMA":
   test "RFC1918 addresses are filterable":
@@ -57,15 +48,15 @@ suite "isFilterablePrivateMA":
 
   test "circuit relay addresses are not filterable even with private relay IP":
     check not isFilterablePrivateMA(
-      ma("/ip4/192.168.1.5/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit")
+      ma(
+        "/ip4/192.168.1.5/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit"
+      )
     )
     check not isFilterablePrivateMA(
-      ma("/ip4/127.0.0.1/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit")
+      ma(
+        "/ip4/127.0.0.1/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit"
+      )
     )
-
-# ---------------------------------------------------------------------------
-# Unit tests: PeerStore.addressPolicy via updatePeerInfo
-# ---------------------------------------------------------------------------
 
 suite "PeerStore addressPolicy":
   test "updatePeerInfo stores all addresses when filter is nil":
@@ -91,12 +82,11 @@ suite "PeerStore addressPolicy":
     peerStore.updatePeerInfo(
       IdentifyInfo(
         peerId: peerId,
-        addrs:
-          @[
-            ma("/ip4/192.168.1.5/tcp/4001"),
-            ma("/ip4/1.1.1.1/tcp/4001"),
-            ma("/ip4/10.0.0.1/tcp/4001"),
-          ],
+        addrs: @[
+          ma("/ip4/192.168.1.5/tcp/4001"),
+          ma("/ip4/1.1.1.1/tcp/4001"),
+          ma("/ip4/10.0.0.1/tcp/4001"),
+        ],
       )
     )
 
@@ -109,7 +99,8 @@ suite "PeerStore addressPolicy":
 
     peerStore.updatePeerInfo(
       IdentifyInfo(
-        peerId: peerId, addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/127.0.0.1/tcp/4001")]
+        peerId: peerId,
+        addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/127.0.0.1/tcp/4001")],
       )
     )
 
@@ -138,12 +129,14 @@ suite "PeerStore addressPolicy":
       peerStore = PeerStore.new(nil)
     peerStore.addressPolicy = publicRoutableAddressPolicy()
 
-    let signedPeerRecord = SignedPeerRecord.init(
-      privKey,
-      PeerRecord.init(
-        peerId, @[ma("/ip4/127.0.0.1/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")]
-      ),
-    ).tryGet()
+    let signedPeerRecord = SignedPeerRecord
+      .init(
+        privKey,
+        PeerRecord.init(
+          peerId, @[ma("/ip4/127.0.0.1/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")]
+        ),
+      )
+      .tryGet()
 
     peerStore.updatePeerInfo(
       IdentifyInfo(
@@ -162,12 +155,14 @@ suite "PeerStore addressPolicy":
       peerStore = PeerStore.new(nil)
     peerStore.addressPolicy = publicRoutableAddressPolicy()
 
-    let signedPeerRecord = SignedPeerRecord.init(
-      privKey,
-      PeerRecord.init(
-        peerId, @[ma("/ip4/127.0.0.1/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")]
-      ),
-    ).tryGet()
+    let signedPeerRecord = SignedPeerRecord
+      .init(
+        privKey,
+        PeerRecord.init(
+          peerId, @[ma("/ip4/127.0.0.1/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")]
+        ),
+      )
+      .tryGet()
 
     peerStore.updatePeerInfo(
       IdentifyInfo(
@@ -184,8 +179,9 @@ suite "PeerStore addressPolicy":
     let peerStore = PeerStore.new(nil)
     peerStore.addressPolicy = publicRoutableAddressPolicy()
 
-    let relayAddr =
-      ma("/ip4/192.168.1.5/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit")
+    let relayAddr = ma(
+      "/ip4/192.168.1.5/tcp/4001/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/p2p-circuit"
+    )
     peerStore.updatePeerInfo(IdentifyInfo(peerId: peerId, addrs: @[relayAddr]))
 
     check peerStore[AddressBook][peerId] == @[relayAddr]
@@ -210,7 +206,14 @@ suite "KadDHT updatePeers address policy":
     switch.mount(kad)
 
     let remotePeer = PeerId.random(rng()).tryGet()
-    kad.updatePeers(@[PeerInfo(peerId: remotePeer, addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")])])
+    kad.updatePeers(
+      @[
+        PeerInfo(
+          peerId: remotePeer,
+          addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")],
+        )
+      ]
+    )
 
     # Use set comparison: extend() uses HashSet internally so order is not guaranteed
     check switch.peerStore[AddressBook][remotePeer].toHashSet() ==
@@ -231,7 +234,14 @@ suite "KadDHT updatePeers address policy":
     switch.mount(kad)
 
     let remotePeer = PeerId.random(rng()).tryGet()
-    kad.updatePeers(@[PeerInfo(peerId: remotePeer, addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")])])
+    kad.updatePeers(
+      @[
+        PeerInfo(
+          peerId: remotePeer,
+          addrs: @[ma("/ip4/192.168.1.5/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")],
+        )
+      ]
+    )
 
     check switch.peerStore[AddressBook][remotePeer] == @[ma("/ip4/1.1.1.1/tcp/4001")]
 
@@ -250,80 +260,12 @@ suite "KadDHT updatePeers address policy":
     switch.mount(kad)
 
     let remotePeer = PeerId.random(rng()).tryGet()
-    kad.updatePeers(@[PeerInfo(peerId: remotePeer, addrs: @[ma("/ip4/192.168.1.5/tcp/4001")])])
+    kad.updatePeers(
+      @[PeerInfo(peerId: remotePeer, addrs: @[ma("/ip4/192.168.1.5/tcp/4001")])]
+    )
 
     check switch.peerStore[AddressBook][remotePeer].len == 0
     check kad.rtable.findClosestPeerIds(remotePeer.toKey(), 1).len == 0
-
-# ---------------------------------------------------------------------------
-# Integration tests: RendezVous filtering
-# ---------------------------------------------------------------------------
-
-proc createRendezVousSwitch(rdv: RendezVous): Switch =
-  SwitchBuilder
-    .new()
-    .withRng(rng)
-    .withAddresses(@[ma(MemoryAutoAddress)])
-    .withMemoryTransport()
-    .withMplex()
-    .withNoise()
-    .withRendezVous(rdv)
-    .build()
-
-suite "RendezVous private address filtering":
-  teardown:
-    checkTrackers()
-
-  asyncTest "server-side filtering omits signed peer records with private addresses":
-    let
-      rendezvousNode = RendezVous.new()
-      requester = RendezVous.new()
-      advertisingPeer = RendezVous.new()
-      rendezvousSwitch = createRendezVousSwitch(rendezvousNode)
-      requesterSwitch = createRendezVousSwitch(requester)
-      advertisingSwitch = createRendezVousSwitch(advertisingPeer)
-
-    discard rendezvousNode.withAddressPolicy(publicRoutableAddressPolicy())
-
-    defer:
-      await advertisingSwitch.stop()
-      await requesterSwitch.stop()
-      await rendezvousSwitch.stop()
-
-    await rendezvousSwitch.start()
-    await requesterSwitch.start()
-    await advertisingSwitch.start()
-
-    await advertisingSwitch.connect(
-      rendezvousSwitch.peerInfo.peerId, rendezvousSwitch.peerInfo.addrs
-    )
-    await requesterSwitch.connect(
-      rendezvousSwitch.peerInfo.peerId, rendezvousSwitch.peerInfo.addrs
-    )
-
-    advertisingSwitch.peerInfo.signedPeerRecord = SignedPeerRecord.init(
-      advertisingSwitch.peerInfo.privateKey,
-      PeerRecord.init(
-        advertisingSwitch.peerInfo.peerId,
-        @[ma("/ip4/127.0.0.1/tcp/4001"), ma("/ip4/1.1.1.1/tcp/4001")],
-      ),
-    ).tryGet()
-
-    const namespace = "filtered-rdv"
-    await advertisingPeer.advertise(namespace)
-
-    let discovered = await rendezvous.request(
-      requester,
-      Opt.some(namespace),
-      Opt.none(int),
-      Opt.some(@[rendezvousSwitch.peerInfo.peerId]),
-    )
-
-    check discovered.len == 0
-
-# ---------------------------------------------------------------------------
-# Integration tests: SwitchBuilder.withPrivateAddressFilter (outbound)
-# ---------------------------------------------------------------------------
 
 suite "SwitchBuilder withPrivateAddressFilter outbound":
   teardown:
@@ -335,7 +277,8 @@ suite "SwitchBuilder withPrivateAddressFilter outbound":
     let switch = SwitchBuilder
       .new()
       .withRng(rng)
-      .withAddresses(@[ma("/ip4/127.0.0.1/tcp/0")], false) # disable wildcard resolver
+      .withAddresses(@[ma("/ip4/127.0.0.1/tcp/0")], false)
+      # disable wildcard resolver
       .withTcpTransport()
       .withMplex()
       .withNoise()
