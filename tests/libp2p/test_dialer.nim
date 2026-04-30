9b6fedca059f3c054582407be9268e2999c7d05d
vladopajic <vladopajic@users.noreply.github.com>
2026-04-30 19:10:20 +0200

    chore(connmanager): naming improvements (#2372)

--- tests/libp2p/test_dialer.nim
+++ tests/libp2p/test_dialer.nim
@@ -32,7 +32,8 @@ suite "Dialer":
   asyncTest "Max connections reached":
     var switches: seq[Switch]
 
-    let dst = newStandardSwitch(limits = Opt.some(LimitsConfig.maxTotal(2)))
+    let dst =
+      newStandardSwitch(connectionLimits = Opt.some(ConnectionLimits.maxTotal(2)))
     await dst.start()
     switches.add(dst)
 
