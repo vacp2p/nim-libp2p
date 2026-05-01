d67ed009a8f265e51544091bac15d9c263b68513
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2026-05-01 19:22:09 -0300

    Update tests/libp2p/kademlia/utils.nim
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/libp2p/kademlia/utils.nim
+++ tests/libp2p/kademlia/utils.nim
@@ -197,12 +197,12 @@ proc sortPeers*(
     peers: seq[PeerId], targetKey: Key, hasher: Opt[XorDHasher]
 ): seq[PeerId] =
   peers
-  .mapIt((it, xorDistance(it, targetKey, hasher)))
-  .sorted(
-    proc(a, b: auto): int =
-      cmp(a[1], b[1])
-  )
-  .mapIt(it[0])
+    .mapIt((it, xorDistance(it, targetKey, hasher)))
+    .sorted(
+      proc(a, b: auto): int =
+        cmp(a[1], b[1])
+    )
+    .mapIt(it[0])
 
 proc addRandomPeers*(
     state: var LookupState, count: int, target: Key, hasher: Opt[XorDHasher]
