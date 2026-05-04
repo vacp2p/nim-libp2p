5453029c2234cb2ce6f62c3d1b02e7964982e703
Vlado Pajić <vlado@brrrrr.at>
2026-05-04 13:39:58 +0200

    improvements

---
+++ tools/check_code_examples.nim
@@ -0,0 +1,33 @@
+import os, osproc, strutils
+
+func isIgnoredRunnableExamplePath(path: string): bool =
+  for ignoredPrefix in [".git", "nimbledeps", "nimcache"]:
+    if path.contains(ignoredPrefix):
+      return true
+  false
+
+func containsRunnableExamples(path: string): bool =
+  {.noSideEffect.}:
+    for line in lines(path):
+      if "runnableExamples" in line:
+        return true
+    false
+
+let outDir = "nimcache/runnable_examples"
+createDir outDir
+
+proc compileFile(file: string) =
+  let cmd = "nim doc" &
+    " -d:libp2p_autotls_support -d:libp2p_mix_experimental_exit_is_dest" &
+    " --index:off --outdir:" & outDir & " " & file
+  echo "Checking runnableExamples in " & file
+  let code = execCmd cmd
+  if code > 0:
+    raise newException(CatchableError, "nim doc failed")
+
+for file in walkDirRec ".":
+  if file.endsWith(".nim") and not isIgnoredRunnableExamplePath(file) and containsRunnableExamples(file):
+    try:
+      compileFile file
+    except CatchableError as e:
+      raiseAssert "failed to compile err: " & e.msg & " file:" & file
