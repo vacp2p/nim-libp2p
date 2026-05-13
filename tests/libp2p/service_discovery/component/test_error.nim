f09879019a2fbebf237f58cc1d343568d21d8f6e
Radoslaw Kaminski <radoslaw@status.im>
2026-05-13 16:46:11 +0100

    add test

---
+++ tests/libp2p/service_discovery/component/test_error.nim
@@ -0,0 +1,128 @@
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+# Copyright (c) Status Research & Development GmbH
+{.used.}
+
+import chronos, results
+import
+  ../../../../libp2p/
+    [protobuf/minprotobuf, protocols/service_discovery/types, stream/connection, switch]
+import ../../../../libp2p/protocols/kademlia/protobuf as kad_protobuf
+import ../../../tools/[lifecycle, unittest]
+import ../utils
+
+proc sendRawMessage(
+    clientSwitch: Switch, registrarNode: ServiceDiscovery, msgBytes: seq[byte]
+): Future[seq[byte]] {.async.} =
+  let conn = await clientSwitch.dial(
+    registrarNode.switch.peerInfo.peerId, registrarNode.switch.peerInfo.addrs,
+    ExtendedServiceDiscoveryCodec,
+  )
+  defer:
+    await conn.close()
+
+  await conn.writeLp(msgBytes)
+
+  return await conn.readLp(4096)
+
+proc sendMessage(
+    clientNode, registrarNode: ServiceDiscovery, msg: kad_protobuf.Message
+): Future[kad_protobuf.Message] {.async.} =
+  let responseBytes =
+    await clientNode.switch.sendRawMessage(registrarNode, msg.encode().buffer)
+  let response = kad_protobuf.Message.decode(responseBytes)
+  check response.isOk()
+  return response.get()
+
+suite "Service Discovery Component - Error Handling":
+  teardown:
+    checkTrackers()
+
+  asyncTest "message with unknown MessageType is rejected without a reply":
+    let registrarNode = setupServiceDiscoveryNode()
+    startAndDeferStop(@[registrarNode])
+
+    let clientSwitch = createSwitch()
+    await clientSwitch.start()
+    defer:
+      await clientSwitch.stop()
+
+    # msgType = 99 is outside the MessageType enum, so decodeEnum will reject it.
+    var pb = initProtoBuffer()
+    pb.write(1, 99'u32)
+    pb.finish()
+    let invalidMsg = pb.buffer
+
+    expect LPStreamError:
+      discard
+        await clientSwitch.sendRawMessage(registrarNode, invalidMsg).wait(2.seconds)
+
+  asyncTest "REGISTER without register body returns Rejected":
+    let registrarNode = setupServiceDiscoveryNode()
+    let clientNode = setupServiceDiscoveryNode()
+    startAndDeferStop(@[registrarNode, clientNode])
+    await connect(registrarNode, clientNode)
+
+    let serviceId = makeServiceId()
+    let msg = kad_protobuf.Message(
+      msgType: kad_protobuf.MessageType.register,
+      key: serviceId,
+      register: Opt.none(kad_protobuf.RegisterMessage),
+    )
+
+    let response = await clientNode.sendMessage(registrarNode, msg)
+    check:
+      response.register.get().status.get() == kad_protobuf.RegistrationStatus.Rejected
+      registrarNode.countAdsInCache(serviceId) == 0
+
+  asyncTest "REGISTER with empty advertisement returns Rejected":
+    let registrarNode = setupServiceDiscoveryNode()
+    let clientNode = setupServiceDiscoveryNode()
+    startAndDeferStop(@[registrarNode, clientNode])
+    await connect(registrarNode, clientNode)
+
+    let serviceId = makeServiceId()
+    let msg = kad_protobuf.Message(
+      msgType: kad_protobuf.MessageType.register,
+      key: serviceId,
+      register: Opt.some(
+        kad_protobuf.RegisterMessage(
+          advertisement: @[],
+          status: Opt.none(kad_protobuf.RegistrationStatus),
+          ticket: Opt.none(kad_protobuf.Ticket),
+        )
+      ),
+    )
+
+    let response = await clientNode.sendMessage(registrarNode, msg)
+    check:
+      response.register.get().status.get() == kad_protobuf.RegistrationStatus.Rejected
+      registrarNode.countAdsInCache(serviceId) == 0
+
+  asyncTest "REGISTER with advertisement for another service returns Rejected":
+    let registrarNode = setupServiceDiscoveryNode()
+    let clientNode = setupServiceDiscoveryNode()
+    startAndDeferStop(@[registrarNode, clientNode])
+    await connect(registrarNode, clientNode)
+
+    let serviceId = "service".hashServiceId()
+    let adBytes = makeAdvertisement(
+        "other-service", clientNode.switch.peerInfo.privateKey
+      )
+      .encode()
+      .get()
+    let msg = kad_protobuf.Message(
+      msgType: kad_protobuf.MessageType.register,
+      key: serviceId,
+      register: Opt.some(
+        kad_protobuf.RegisterMessage(
+          advertisement: adBytes,
+          status: Opt.none(kad_protobuf.RegistrationStatus),
+          ticket: Opt.none(kad_protobuf.Ticket),
+        )
+      ),
+    )
+
+    let response = await clientNode.sendMessage(registrarNode, msg)
+    check:
+      response.register.get().status.get() == kad_protobuf.RegistrationStatus.Rejected
+      registrarNode.countAdsInCache(serviceId) == 0
