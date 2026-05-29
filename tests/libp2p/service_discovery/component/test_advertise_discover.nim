b9a08792f73a10a8d4782ec08ace0f6176190eef
Simon-Pierre <simvivier@status.im>
2026-05-29 08:47:10 -0400

    remove redundant test

--- tests/libp2p/service_discovery/component/test_advertise_discover.nim
+++ tests/libp2p/service_discovery/component/test_advertise_discover.nim
@@ -112,42 +112,6 @@ suite "Service Discovery Component - Advertise Discover":
         found.get().len == 2 and found.containsPeer(advertiserA) and
           found.containsPeer(advertiserB)
 
-  asyncTest "one advertiser provides two services - both discoverable":
-    # TODO: vacp2p/nim-libp2p#2430 service-disco: missing API for multi-service registration
-    let conf = ServiceDiscoveryConfig.new(safetyParam = 0.0, ipSimCoefficient = 0.0)
-    let registrarNode = setupServiceDiscoveryNode(discoConfig = conf)
-    let advertiserNode = setupServiceDiscoveryNode(discoConfig = conf)
-    let discovererNode = setupServiceDiscoveryNode(discoConfig = conf)
-    startAndDeferStop(@[registrarNode, advertiserNode, discovererNode])
-
-    await connectHub(registrarNode, @[advertiserNode, discovererNode])
-
-    let svcA = makeServiceInfo("service-A")
-    let svcB = makeServiceInfo("service-B")
-    let svcAId = svcA.id.hashServiceId()
-    let svcBId = svcB.id.hashServiceId()
-
-    advertiserNode.addProvidedService(svcA)
-
-    checkUntilTimeout:
-      registrarNode.countAdsInCache(svcAId) == 1
-
-    advertiserNode.addProvidedService(svcB)
-
-    checkUntilTimeout:
-      registrarNode.countAdsInCache(svcBId) == 1
-
-    checkUntilTimeout:
-      block:
-        let foundA = await discovererNode.lookup(svcAId)
-        foundA.get().anyIt(it.data.peerId == advertiserNode.switch.peerInfo.peerId) and
-          foundA.get().len == 1
-      block:
-        let foundB = await discovererNode.lookup(svcBId)
-        foundB.get().anyIt(it.data.peerId == advertiserNode.switch.peerInfo.peerId) and
-          foundB.get().len == 1
-          # Regression for vacp2p/nim-libp2p#2431: this lookup previously returned a duplicate.
-
   asyncTest "lookup dedups byte-identical adverts but keeps same (peerId, seqNo) variants":
     let conf = ServiceDiscoveryConfig.new(safetyParam = 0.0)
     let registrar1 = setupServiceDiscoveryNode(discoConfig = conf)
@@ -328,9 +292,7 @@ suite "Service Discovery Component - Advertise Discover":
     check advertiserNode.countAdsInCache(serviceId) == 0
 
   asyncTest "auto-advertise on start":
-    # Using ipSimCoefficient = 0.0 to not trigger sybil protection when advertising multiple services
-    # TODO: vacp2p/nim-libp2p#2430 service-disco: missing API for multi-service registration
-    let conf = ServiceDiscoveryConfig.new(safetyParam = 0.0, ipSimCoefficient = 0.0)
+    let conf = ServiceDiscoveryConfig.new()
     let registrarNode = setupServiceDiscoveryNode(discoConfig = conf)
     let discovererNode = setupServiceDiscoveryNode(discoConfig = conf)
 
