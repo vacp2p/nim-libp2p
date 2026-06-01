3788eed50111d94a8e3cbbbec224a67463c09e34
Vlado Pajić <vlado@brrrrr.at>
2026-06-01 15:01:30 +0200

    cosmetics

--- tests/libp2p/test_protocol.nim
+++ tests/libp2p/test_protocol.nim
@@ -16,7 +16,7 @@ template assertNoLimits(p: LPProtocol) =
   # when there are no limits reserve and release will always succeed
 
   # this for is here to drive over any limits (default or explicit)
-  for i in 0 .. 1000:
+  for _ in 0 .. 1000:
     check:
       p.reserveIncoming(peerId1)
       p.reserveOutgoing(peerId1)
