632e3c97d14eb1aac1e91d67ae44363762e3653a
Radosław Kamiński <radoslaw@status.im>
2026-07-03 11:56:16 +0100

    test(autonat): dual stack 4 (#2733)

--- tests/tools/switch_builder.nim
+++ tests/tools/switch_builder.nim
@@ -8,27 +8,41 @@ import ./[crypto, multiaddress]
 
 export builders
 
+proc makeStandardSwitchBuilder*(addresses: seq[MultiAddress]): SwitchBuilder =
+  ## Helper that creates a SwitchBuilder with standard configurations.
+  ## Transports are added automatically to match the listen `addresses`.
+
+  var b = SwitchBuilder.new().withRng(rng()).withNoise().withAddresses(addresses)
+
+  # the listen addresses decide which transports to use, deduped across addresses
+  var hasQuic, hasTcp, hasWs, hasMemory = false
+  for address in addresses:
+    if QUIC_V1.match(address):
+      hasQuic = true
+    elif TCP.match(address):
+      hasTcp = true
+    elif WebSockets.match(address):
+      hasWs = true
+    elif Memory.match(address):
+      hasMemory = true
+    else:
+      raiseAssert "could not infer transport from address: " & $address
+
+  if hasQuic:
+    b = b.withQuicTransport()
+  if hasTcp:
+    b = b.withTcpTransport().withMplex()
+  if hasWs:
+    b = b.withWsTransport().withMplex()
+  if hasMemory:
+    b = b.withMemoryTransport().withMplex()
+
+  b
+
 proc makeStandardSwitchBuilder*(
     address: MultiAddress = QuicAutoAddress
 ): SwitchBuilder =
-  ## Helper that creates a SwitchBuilder with standard configurations.
-  ## Transport is added automatically to match the listen `address`.
-
-  var b = SwitchBuilder.new().withRng(rng()).withNoise().withAddress(address)
-
-  # address will decide which transport to use
-  if QUIC_V1.match(address):
-    b = b.withQuicTransport()
-  elif TCP.match(address):
-    b = b.withTcpTransport().withMplex()
-  elif WebSockets.match(address):
-    b = b.withWsTransport().withMplex()
-  elif Memory.match(address):
-    b = b.withMemoryTransport().withMplex()
-  else:
-    raiseAssert "could not infer transport from address"
-
-  b
+  makeStandardSwitchBuilder(@[address])
 
 proc makeStandardSwitch*(
     address: MultiAddress = QuicAutoAddress
