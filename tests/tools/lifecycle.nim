ba5624b13500335c734f73bd7be8abb8f1a0ce7a
Vlado Pajić <vlado@brrrrr.at>
2026-07-10 15:25:46 +0200

    revert

--- tests/tools/lifecycle.nim
+++ tests/tools/lifecycle.nim
@@ -8,29 +8,29 @@ import ../../libp2p/switch
 import ./futures
 
 proc startNodes*(nodes: seq[Switch]) {.async.} =
-  chronos.await(allFuturesRaising(nodes.mapIt(it.start())))
+  await allFuturesRaising(nodes.mapIt(it.start()))
 
 proc stopNodes*(nodes: seq[Switch]) {.async.} =
-  chronos.await(allFuturesRaising(nodes.mapIt(it.stop())))
+  await allFuturesRaising(nodes.mapIt(it.stop()))
 
 template startAndDeferStop*(nodes: seq[Switch]): untyped =
-  chronos.await(startNodes(nodes))
+  await startNodes(nodes)
   defer:
-    chronos.await(stopNodes(nodes))
+    await stopNodes(nodes)
 
 proc startNodes*[T](nodes: seq[T]) {.async.} =
-  chronos.await(startNodes(nodes.mapIt(it.switch)))
+  await startNodes(nodes.mapIt(it.switch))
 
   when compiles(nodes[0].start()):
-    chronos.await(allFuturesRaising(nodes.mapIt(it.start())))
+    await allFuturesRaising(nodes.mapIt(it.start()))
 
 proc stopNodes*[T](nodes: seq[T]) {.async.} =
   when compiles(nodes[0].stop()):
-    chronos.await(allFuturesRaising(nodes.mapIt(it.stop())))
+    await allFuturesRaising(nodes.mapIt(it.stop()))
 
-  chronos.await(stopNodes(nodes.mapIt(it.switch)))
+  await stopNodes(nodes.mapIt(it.switch))
 
 template startAndDeferStop*[T](nodes: seq[T]): untyped =
-  chronos.await(startNodes(nodes))
+  await startNodes(nodes)
   defer:
-    chronos.await(stopNodes(nodes))
+    await stopNodes(nodes)
