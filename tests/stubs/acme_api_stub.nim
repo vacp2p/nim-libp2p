d976405a899f4700416111cac30d6c153c8e281a
Radoslaw Kaminski <radoslaw@status.im>
2026-08-14 13:46:54 +0100

    refactor

--- tests/stubs/acme_api_stub.nim
+++ tests/stubs/acme_api_stub.nim
@@ -7,43 +7,21 @@
 
 import uri
 import chronos, chronos/apps/http/httpclient
-import ../../libp2p/[autotls/acme/api, utils/opt]
+import ../../libp2p/autotls/acme/api
 
 export api
 
 type ACMEApiStub* = ref object of ACMEApi
   ## Refuses every ACME request, recording what was requested.
-  stalls*: bool
   requestedUris*: seq[Uri]
 
 proc new*(T: typedesc[ACMEApiStub]): ACMEApiStub =
-  ACMEApiStub(
-    session: HttpSessionRef.new(),
-    directory: Opt.some(
-      ACMEDirectory(
-        newNonce: LetsEncryptURL & "/new-nonce",
-        newOrder: LetsEncryptURL & "/new-order",
-        newAccount: LetsEncryptURL & "/new-account",
-      )
-    ),
-    acmeServerURL: parseUri(LetsEncryptURL),
-  )
-
-method requestNonce*(
-    self: ACMEApiStub
-): Future[Nonce] {.async: (raises: [ACMEError, CancelledError]).} =
-  Nonce($self.acmeServerURL & "/acme/1234")
-
-proc stallUntilCancelled(self: ACMEApiStub) {.async: (raises: [CancelledError]).} =
-  ## A request that only ends when the caller gives up on it.
-  await Future[void].Raising([CancelledError]).init("ACMEApiStub.request")
+  ACMEApiStub(session: HttpSessionRef.new(), acmeServerURL: parseUri(LetsEncryptURL))
 
 proc refuse(
     self: ACMEApiStub, uri: Uri
 ): Future[HTTPResponse] {.async: (raises: [ACMEError, CancelledError]).} =
   self.requestedUris.add(uri)
-  if self.stalls:
-    await self.stallUntilCancelled()
   raise newException(ACMEError, "ACMEApiStub refused " & $uri)
 
 method post*(
