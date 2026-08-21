f33df1fbe9ffa702b67c1c71ed367a1823beae1d
vlado <vlado@brrrrr.at>
2026-08-21 15:16:10 +0200

    fmt

--- libp2p/protocols/pubsub/rpc/messages.nim
+++ libp2p/protocols/pubsub/rpc/messages.nim
@@ -488,12 +488,9 @@ func anonymize*(msg: RPCMsg, anonymize: bool): RPCMsg =
     msg
 
 func hasOnlyMessages*(msg: RPCMsg): bool =
-  msg.subscriptions.len == 0 and
-    msg.control.isNone and
-    msg.partialMessageExtension.isNone and
-    msg.testExtension.isNone and
-    msg.pingpongExtension.isNone and
-    msg.preambleExtension.isNone
+  msg.subscriptions.len == 0 and msg.control.isNone and
+    msg.partialMessageExtension.isNone and msg.testExtension.isNone and
+    msg.pingpongExtension.isNone and msg.preambleExtension.isNone
 
 func validate*(sub: SubOpts): Result[void, string] =
   if sub.topic.isNone:
