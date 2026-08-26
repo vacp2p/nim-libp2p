bc96175df0a6253e1fd0aa035cc3823c59d83e9d
Radoslaw Kaminski <radoslaw@status.im>
2026-08-26 12:43:06 +0100

    add comment

--- tests/libp2p/test_dialer.nim
+++ tests/libp2p/test_dialer.nim
@@ -54,6 +54,7 @@ suite "Dialer":
     await allFutures(src.stop(), dst.stop())
 
   asyncTest "Max connections reached":
+    # TODO: vacp2p/nim-lsquic#162
     var switches: seq[Switch]
 
     let dst = makeStandardSwitchBuilder()
