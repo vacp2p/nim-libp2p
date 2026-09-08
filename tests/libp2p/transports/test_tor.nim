c3f9066d0e3594993fa55d5a51538dbff665a5c1
Richard Ramos <info@richardramos.me>
2026-09-08 16:37:29 -0400

    test(tor): cover truncated SOCKS5 CONNECT replies

--- tests/libp2p/transports/test_tor.nim
+++ tests/libp2p/transports/test_tor.nim
@@ -242,3 +242,47 @@ suite "Tor authentication cleanup":
     await peer.shutdownWait()
     expect TransportDialError:
       discard await dialing
+
+suite "Tor CONNECT reply cleanup":
+  teardown:
+    checkTrackers()
+
+  proc checkTruncatedReply(response: seq[byte]) {.async.} =
+    let server = createStreamServer(initTAddress("127.0.0.1:0"))
+    let transport = TorTransport.new(server.localAddress, upgrade = Upgrade())
+    let dialing = transport.dial("", ma("/ip4/127.0.0.1/tcp/1234"))
+    let peer = await server.accept()
+    defer:
+      await peer.closeWait()
+      await server.closeWait()
+      await transport.stop()
+    var greeting: array[3, byte]
+    await peer.readExactly(addr greeting[0], greeting.len)
+    discard await peer.write(@[5'u8, 0])
+    var request: array[10, byte]
+    await peer.readExactly(addr request[0], request.len)
+    discard await peer.write(response)
+    # Signal EOF while keeping the read side open to observe client cleanup.
+    await peer.shutdownWait()
+    expect TransportDialError:
+      discard await dialing
+    var reply: array[1, byte]
+    check (await peer.readOnce(addr reply[0], 1).wait(100.millis)) == 0
+
+  asyncTest "truncated CONNECT header closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0])
+
+  asyncTest "missing CONNECT FQDN length closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0, 3])
+
+  asyncTest "truncated CONNECT IPv4 address closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0, 1, 127, 0, 0])
+
+  asyncTest "truncated CONNECT IPv4 port closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0, 1, 127, 0, 0, 1, 0])
+
+  asyncTest "truncated CONNECT IPv6 port closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0, 4] & newSeq[byte](17))
+
+  asyncTest "truncated CONNECT FQDN port closes the proxy socket":
+    await checkTruncatedReply(@[5'u8, 0, 0, 3, 3, 97, 98, 99, 0])
