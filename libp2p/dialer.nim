b3620e7b0c8648da072515d26aeb4e5ecc4bfdf2
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2026-09-09 17:25:13 -0300

    Update libp2p/dialer.nim
    Co-authored-by: vladopajic <vladopajic@users.noreply.github.com>

--- libp2p/dialer.nim
+++ libp2p/dialer.nim
@@ -322,7 +322,7 @@ proc expandCandidate(
         address: address,
         hostname: address.getHostname(),
         peerId: addrPeerId,
-        fromName: fromName,
+        fromName: candidate.fromNameLookup(),
       )
     )
 
