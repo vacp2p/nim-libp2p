8e5dd920c2d8e6ddbd82c7a53588508c39b44a95
Richard Ramos <info@richardramos.me>
2026-09-22 10:56:39 -0400

    fix(quic): reuse dual-stack listeners cross-platform

--- libp2p/transports/quictransport.nim
+++ libp2p/transports/quictransport.nim
@@ -616,14 +616,13 @@ proc listenerEndpointFor(
   if matchedEndpoint.isSome() or address.family != AddressFamily.IPv4:
     return ok(matchedEndpoint)
 
-  when defined(linux):
-    for endpoint in self.listeners:
-      let local = endpoint.localAddress()
-      if local.family == AddressFamily.IPv6 and local.isAnyLocal() and
-          endpoint.datagramTransport().fd.getDualstack().get(false):
-        if matchedEndpoint.isSome():
-          return ok(Opt.none(QuicEndpoint))
-        matchedEndpoint = Opt.some(endpoint)
+  for endpoint in self.listeners:
+    let local = endpoint.localAddress()
+    if local.family == AddressFamily.IPv6 and local.isAnyLocal() and
+        endpoint.datagramTransport().fd.getDualstack().get(false):
+      if matchedEndpoint.isSome():
+        return ok(Opt.none(QuicEndpoint))
+      matchedEndpoint = Opt.some(endpoint)
 
   ok(matchedEndpoint)
 
